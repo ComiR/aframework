e7e91c3615b1cbdcf514b63c8e1241ea6e6bbeb1
Andreas Lagerkvist <powerbuoy@gmail.com>
2008-02-12 14:15:52 +0000

    Captcha-class started. No compression in JS because decompression is slower than downloading large file (same size gzipped anyway)

--- __core/Lib/Captcha.php
+++ __core/Lib/Captcha.php
@@ -1,35 +1,47 @@
 <?php
-	require_once(str_replace("//", "/", $_SERVER['DOCUMENT_ROOT'] ."/") ."__core/Config.php");
-
 	session_start();
+	$c = new Captcha(str_replace('//', '/', $_SERVER['DOCUMENT_ROOT'] .'/') .'__files/captcha.ttf', 4);
+	$c->show();
 
-	function make_word($len = 4)
-	{
-		$chars = 'abcdefghijklmnopqrstuvwxyz';
-		$word = '';
-		for($i=0; $i<intval($len); $i++)
-		{
-			$word .= substr($chars, rand(0, strlen($chars) -1), 1);
+	/**
+	 * Generates a "captcha"
+	 * 
+	 * @class Captcha
+	 */
+	class Captcha {
+		private $font = false;
+		private $length = 4;
+
+		public function __construct($f, $l) {
+			if(file_exists($f)) {
+				$this->font = $f;
+			}
+			if(is_numeric($l)) {
+				$this->lenght = $l;
+			}
 		}
-		if(!empty($word)) return $word;
 
-		return false;
+		public function show() {
+			header('Content-type: image/png');
+
+			$str = $this->genStr(); 
+			$_SESSION['captcha'] = md5($str);
+			$im = imagecreate(60, 30);
+			$bg = imagecolorallocate($im, 255, 255, 255);
+			$clr = imagecolorallocate($im, 33, 66, 99);
+
+			imagettftext($im, 20, 0, 5, 20, $clr, $this->font, $str);
+			imagepng($im);
+			imagedestroy($im);
+		}
+
+		private function genStr() {
+			$chars = 'abcdefghijklmnopqrstuvwxyz';
+			$word = '';
+			for($i = 0; $i < $this->length; $i++) {
+				$word .= substr($chars, rand(0, strlen($chars) -1), 1);
+			}
+			return $word;
+		}
 	}
-
-	$str = make_word(); 
-	$_SESSION['captcha'] = md5($str);
-
-	header("Content-type: image/png");
-
-	$im = imagecreate(60, 30);
-
-	$bg = imagecolorallocate($im, 255, 255, 255);
-	$clr = imagecolorallocate($im, 33, 66, 99);
-
-	$font = FILES_DIR ."misc/fonts/captcha.ttf";
-
-	imagettftext($im, 20, 0, 5, 20, $clr, $font, $str);
-
-	imagepng($im);
-	imagedestroy($im);
 ?>
