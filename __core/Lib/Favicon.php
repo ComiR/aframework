6bb2c86751324f3ad79dc2a2c4dfcf4d0dbd79b2
Andreas Lagerkvist <powerbuoy@gmail.com>
2008-02-27 15:10:18 +0000

    Added RSSFeed class and replaced old favicon-stuff wiv new class

--- __core/Lib/Favicon.php
+++ __core/Lib/Favicon.php
@@ -1,123 +1,72 @@
 <?php
-	/*
-		function getSiteFavicon($url)
-		{
-			return HTML_FILES_DIR ."misc/favicons/no-favicon.png"; // comment this when internet connection is available
-			$favicon = "";
+	/**
+	 * Takes a URL and returns that sites favicon's URL (if any)
+	 *
+	 * @class Favicon
+	 */
+	class Favicon {
+		private $url;
+		private $cachedURLs = array();
+		private $cachedURLsFile = 'lib/cachedurls';
+		private $defaultFaviconFile = 'lib/favicon.png';
 
-			if(@file_get_contents($url ."/favicon.ico"))
-			{
-				$favicon = $url ."/favicon.ico";
+		public function __construct($url) {
+			$this->loadCachedURLs();
+
+			$this->url = $url;
+		}
+
+		public function getFaviconURL() {
+			if(strlen($this->url) < 5) { // can't be real url
+				return $this->defaultFaviconFile;
 			}
-			else
-			{
-				$subject = file_get_contents($url);
-				$pattern = "/[^\"]*\.ico/i";
+
+			$this->_getFaviconURL();
+
+			return $this->cachedURLs["$this->url"]; // weird, "" are needed else "index does not exist" (same below)
+		}
+
+		private function _getFaviconURL() {
+			// Don't fetch it again if already cached
+			if(isset($this->cachedURLs["$this->url"])) {
+				return;
+			}
+
+			// See if favicon.ico exists in root of target site
+			if(@file_get_contents($this->url .'/favicon.ico')) {
+				$favicon = $this->url .'/favicon.ico';
+			}
+			// If not, search for first occurence of *.ico in site source
+			else {
+				$str = @file_get_contents($this->url);
+				$find = '/[^"]*\.ico/i';
 				$matches = array();
-				
-				if(preg_match($pattern, $subject, $matches))
-				{
-					$favicon = $matches[0];
+
+				if(preg_match($find, $str, $matches)) {
+					$favicon = (stristr($matches[0], 'http://')) ? $matches[0] : $this->url .$matches[0]; // Possibly takes care of relative paths
 				}
-				else
-				{
-					$favicon = HTML_FILES_DIR ."misc/favicons/no-favicon.png";
+				else {
+					$favicon = $this->defaultFaviconFile;
 				}
 			}
 
+			// Make sure there are no double slashes (except the first two)
 			$favicon = str_replace('http:/', 'http://', preg_replace('/\/+/', '/', $favicon));
 
-			return $favicon;
+			$this->cachedURLs["$this->url"] = $favicon;
+			$this->saveCachedURLs();
 		}
-	*/
 
-	require_once(str_replace("//", "/", $_SERVER['DOCUMENT_ROOT'] ."/") ."__core/Config.php");
-
-	// Gets a site's favicon from cache, or if not in cache, from server
-	$url = (isset($_GET['url'])) ? str_replace(array("..\\", "../"), "", $_GET['url']) : "www.exscale.se";
-
-	if($url)
-	{
-		$fi = FILES_DIR ."misc/favicons/" .$url .".ico";
-		
-		// If not in cache
-		if(!file_exists($fi))
-		{
-			echo "$fi not in cache, starting search<br />";
-
-			$rootIco = @file_get_contents($url ."/favicon.ico");
-
-			// Check in root
-			if($rootIco)
-			{
-				echo "Favicon in root dir, saving to disk<br />";
-
-				file_put_contents(FILES_DIR ."misc/favicons/" .$url .".ico", $rootIco);
+		private function loadCachedURLs() {
+			if(!file_exists($this->cachedURLsFile)) {
+				file_put_contents($this->cachedURLsFile, serialize(array()));
 			}
-			// Search for it
-			else
-			{
-				echo "Favicon not in root, parsing index-file for ico-linking<br />";
 
-				$subject = @file_get_contents($url);
-				$pattern = "/[^\"]*\.ico/i";
-				$matches = array();
-				
-				if(preg_match($pattern, $subject, $matches))
-				{
-					echo "Favicon found in code, check that " .str_replace("//", "/", $url .$matches[0]) ." really exists<br />";
-
-					$codeIco = @file_get_contents(str_replace("//", "/", "http://" .$url .$matches[0]));
-
-					if($codeIco)
-					{
-						echo "Favicon found in code, saving to disk<br />";
-
-						$ext = end(explode(".", $matches[0]));
-
-						file_put_contents(FILES_DIR ."misc/favicons/" .$url ."." .$ext, $codeIco);
-					}
-					// There is no favicon
-					else
-					{
-						echo "No favicon found, saving the no-favicon one<br />";
-
-						copy(FILES_DIR ."misc/favicons/no-favicon.png", FILES_DIR ."misc/favicons/" .$url .".png");
-					}
-				}
-				// There is no favicon
-				else
-				{
-					echo "No favicon found, saving the no-favicon one<br />";
-
-					copy(FILES_DIR ."misc/favicons/no-favicon.png", FILES_DIR ."misc/favicons/" .$url .".png");
-				}
-			}
+			$this->cachedURLs = unserialize(stripslashes(file_get_contents($this->cachedURLsFile)));
 		}
 
-		die();
-
-		// Display it
-		$ext = end(explode(".", $fi));
-
-		switch($ext)
-		{
-			case "ico" : 
-				header("Content-type: image/ico");
-				break;
-			case "jpg" : 
-				header("Content-type: image/ico");
-				break;
-			case "png" : 
-				header("Content-type: image/ico");
-				break;
-			case "gif" : 
-				header("Content-type: image/ico");
-				break;
-			case "bmp" : 
-				header("Content-type: image/ico");
-				break;
+		private function saveCachedURLs() {
+			file_put_contents($this->cachedURLsFile, serialize($this->cachedURLs));
 		}
-		readfile($fi);
 	}
 ?>
