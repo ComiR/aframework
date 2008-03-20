dea0184401b6a6ab8c4be830966fdd0af95b03e9
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2008-03-20 16:17:41 +0000

    Replaced real JS with jQuery (getAttribute etc)

--- trunk/__core/Modules/Base/jquery.imgzoom.js
+++ trunk/__core/Modules/Base/jquery.imgzoom.js
@@ -18,29 +18,25 @@ jQuery.imgzoom = function(conf) {
 		hideClicked: 1	// Whether to hide the image that was clicked to bring up the zoomed version
 	};
 	config = jQuery.extend(config, conf);
-	config.doubleSpeed= config.speed / 4;
+	config.doubleSpeed = config.speed / 4;
 
 	// Let events bubble up to body (supports dynamically inserted links)
 	jQuery(document.body).click(function(event) {
-		// Get the clicked element
-		var clickedElement = event.target;
+		// Get the clicked element (it may be an image inside a link)
+		var clickedElement = jQuery(event.target);
+		clickedElement = clickedElement.is('a') ? clickedElement : clickedElement.parents('a');
 
-		// If the clicked element is not an anchor, see if any of its parents is
-		if(clickedElement.nodeName.toLowerCase() != 'a') {
-			clickedElement = $(clickedElement).parents('a')[0];
-		}
-
-		// If clicked element (or any of its parents) has an 'href'-attribute matching the image-reg-exp, continue
+		// If clicked element has an 'href'-attribute matching the image-reg-exp, continue
 		if(
 			clickedElement && 
-			clickedElement.nodeName.toLowerCase() == 'a' && 
-			clickedElement.getAttribute('href', 2).search(/(.*)\.(jpg|jpeg|gif|png|bmp|tif|tiff)/gi) != -1
+			clickedElement.is('a') && 
+			clickedElement.attr('href').search(/(.*)\.(jpg|jpeg|gif|png|bmp|tif|tiff)/gi) != -1
 		) {
 			// Get the href (that points to the image)
-			var imgSrc = clickedElement.getAttribute('href', 2);
+			var imgSrc = clickedElement.attr('href');
 
 			// If it's alredy open, do nothing
-			if($('div.imgzoom img[src="' +imgSrc +'"]').length) {
+			if(jQuery('div.imgzoom img[src="' +imgSrc +'"]').length) {
 				return false;
 			}
 
@@ -48,26 +44,26 @@ jQuery.imgzoom = function(conf) {
 			var hideClicked = 0;
 
 			// See if link contains an image, if so get dimensions from the image rather than the link
-			var e = jQuery(clickedElement).find('img');
-			if(e.length) {
+			var tmpElement = clickedElement.find('img');
+			if(tmpElement.length) {
 				hideClicked = config.hideClicked;
 			}
 			else {
-				e = jQuery(clickedElement);
+				tmpElement = clickedElement;
 			}
 
-			// Get the clicked element's dimensions
-			var offset = e.offset();
+			// Get the clicked element's dimensions (imgzoom will animate from this size to its real size)
+			var offset = tmpElement.offset();
 			var oldDim = {
-				width: e.outerWidth(), 
-				height: e.outerHeight(), 
+				width: tmpElement.outerWidth(), 
+				height: tmpElement.outerHeight(), 
 				left: offset.left, 
 				top: offset.top, 
 				opacity: config.dontFadeIn
 			};
 
 			if(hideClicked) {
-				jQuery(clickedElement).css({visibility: 'hidden'});
+				clickedElement.css({visibility: 'hidden'});
 			}
 
 			// This function animates and displays the imgzoom-div
@@ -75,13 +71,17 @@ jQuery.imgzoom = function(conf) {
 				// Append image to body
 				var imgzoom = jQuery('<div><img src="' +imgSrc +'" alt="" /></div>').css({position: 'absolute'}).appendTo(document.body);
 
-				// Get its dimensions
+				// Get the image's dimensions as well as the windows scroll-pos/dimensions
 				var width = imgzoom.outerWidth();
 				var height = imgzoom.outerHeight();
-				var left = (jQuery(window).width() - width) / 2 + jQuery(window).scrollLeft();
-				var	top = (jQuery(window).height() - height) / 2 + jQuery(window).scrollTop();
+				var windowHeight = jQuery(window).height();
+				var windowWidth = jQuery(window).width();
+				var scrollLeft = jQuery(window).scrollLeft();
+				var scrollTop = jQuery(window).scrollTop();
+				var left = (windowWidth - width) / 2 + scrollLeft;
+				var	top = (windowHeight - height) / 2 + scrollTop;
 
-				// These are the dimensions of the imgzoom
+				// These are the dimensions of the imgzoom when zoomed out
 				var newDim = {
 					width: width,
 					height: height, 
@@ -91,16 +91,19 @@ jQuery.imgzoom = function(conf) {
 					overflow: 'auto'
 				};
 
-				// Now add close-button and stuff (we didn't want them in the dimensions-calculation)
+				// Now add close-button (we didn't want it in the dimensions-calculationas it's positioned absolutely anyway)
 				var closeButton = jQuery('<a href="#">Close</a>').appendTo(imgzoom).hide();
 
-				// Set imgzoom's dimensions to clicked element's and animate to new
+				// Add imgzoom-class, imgzoom's dimensions to clicked element's and animate to new
 				imgzoom.addClass('imgzoom').css(oldDim).animate(newDim, config.speed, function() {
+					// Now center the imgzoom fixed (so it follows when you scroll)
 				//	imgzoom.center();
+
+					// Fade in the close-button (we do this because it looks ugly if shown during imgzoom animation due to overflow set to hidden)
 					closeButton.fadeIn(config.doubleSpeed);
 
 					// This replaces the image with a youtube-clip if the clicked link has a class of youtube-.*
-					var classNames = jQuery(clickedElement).attr('class');
+					var classNames = clickedElement.attr('class');
 
 					if(classNames) {
 						classNames = classNames.split(' ');
@@ -133,7 +136,7 @@ jQuery.imgzoom = function(conf) {
 							imgzoom.remove();
 
 							if(hideClicked) {
-								jQuery(clickedElement).css({visibility: 'visible'});
+								clickedElement.css({visibility: 'visible'});
 							}
 						});
 					});
