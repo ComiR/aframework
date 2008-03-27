77f4cdd6e40583317d3bd7b24c8b8043eb8ca3e0
Andreas Lagerkvist <powerbuoy@gmail.com>
2008-03-27 16:17:28 +0000

    Added vibrate plugin

---
+++ __core/Modules/Base/jquery.vibrate.js
@@ -0,0 +1,46 @@
+/**
+ * Vibrate 1.0
+ *
+ * Makes an element vibrate
+ *
+ * Usage: $('#my-annoying-ad').vibrate();
+ *
+ * @class vibrate
+ * @param {Object} conf, custom config-object
+ *
+ * Copyright (c) 2008 Andreas Lagerkvist (andreaslagerkvist.com)
+ * Released under a GNU General Public License v3 (http://creativecommons.org/licenses/by/3.0/)
+ */
+jQuery.fn.vibrate = function(conf) {
+	var config = {
+		speed: 30, 
+		duration: 2000, 
+		frequency: 10000, 
+		spread: 3
+	};
+
+	config = jQuery.extend(config, conf);
+
+	return this.each(function() {
+		var t = $(this);
+
+		var vibrate = function() {
+			var topPos = Math.floor(Math.random() * config.spread) - ((config.spread - 1) / 2);
+			var leftPos = Math.floor(Math.random() * config.spread) - ((config.spread - 1) / 2);
+			t.css({position: 'relative', left: leftPos +'px', top: topPos +'px'});
+		};
+
+		var doVibration = function () {
+			var vibrationInterval = setInterval(vibrate, config.speed);
+
+			var stopVibration = function() {
+				clearInterval(vibrationInterval);
+				t.css({position: 'static'});
+			};
+
+			setTimeout(stopVibration, config.duration);
+		};
+
+		setInterval(doVibration, config.frequency);
+	});
+};
