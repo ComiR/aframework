89474bea4a94e3b77dc181001d90c313772d8a73
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2008-08-11 15:21:53 +0000

    Deleted App-site

--- trunk/__v3/App/Modules/CSSCompressor/CSSCompressorModule.php
+++
@@ -1,9 +0,0 @@
-<?php
-	class App_CSSCompressorModule extends aFramework_CSSCompressorModule {
-		public static function run() {
-			self::$exclude = array('print.css');
-
-			parent::run();
-		}
-	}
-?>
