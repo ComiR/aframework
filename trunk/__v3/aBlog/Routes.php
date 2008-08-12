1b0d8365053899883806c14d6c836fd9a92f2c98
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2008-08-12 22:32:15 +0000

    Removed page-url from routes. Inheriting from aCMS

--- trunk/__v3/aBlog/Routes.php
+++ trunk/__v3/aBlog/Routes.php
@@ -7,7 +7,6 @@
 		'/archives/:year([0-9]{4,4})/:month([0-9]{2,2})/'								=> 'ArticleListing', 
 		'/archives/:year([0-9]{4,4})/:month([0-9]{2,2})/:day([0-9]{2,2})/'				=> 'ArticleListing', 
 		'/archives/:year([0-9]{4,4})/:month([0-9]{2,2})/:day([0-9]{2,2})/:url_str/'		=> 'Article', 
-		'/archives/:url_str/'															=> 'ArticleListing',
-		'/:url_str/'																	=> 'Page'
+		'/archives/:url_str/'															=> 'ArticleListing'
 	);
 ?>
