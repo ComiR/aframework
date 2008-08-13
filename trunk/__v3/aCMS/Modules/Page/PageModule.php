60838c9bdd363659047b6b5370813419a0fbf774
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2008-08-13 22:20:09 +0000

    Added home-controller and default to 'home'-page in PageModule if no url_str is set

--- trunk/__v3/aCMS/Modules/Page/PageModule.php
+++ trunk/__v3/aCMS/Modules/Page/PageModule.php
@@ -5,21 +5,16 @@
 		public static $forceController = false;
 
 		public static function run() {
-			if(isset($_GET['url_str'])) {
-				$page = Pages::getPageByUrlStr($_GET['url_str']);
+			$page = Pages::getPageByUrlStr(isset($_GET['url_str']) ? $_GET['url_str'] : 'home');
 
-				if(!$page) {
-					self::$forceController = FOUR_O_FOUR_CONTROLLER;
-				}
-				else {
-					self::$tplVars = $page;
-					aFramework_BaseModule::$tplVars['html_title'] = $page['title'];
-					aFramework_BaseModule::$tplVars['meta_description'] = $page['meta_description'];
-					aFramework_BaseModule::$tplVars['meta_keywords'] = $page['meta_keywords'];
-				}
+			if(!$page) {
+				self::$forceController = FOUR_O_FOUR_CONTROLLER;
 			}
 			else {
-				self::$forceController = FOUR_O_FOUR_CONTROLLER;
+				self::$tplVars = $page;
+				aFramework_BaseModule::$tplVars['html_title'] = $page['title'];
+				aFramework_BaseModule::$tplVars['meta_description'] = $page['meta_description'];
+				aFramework_BaseModule::$tplVars['meta_keywords'] = $page['meta_keywords'];
 			}
 		}
 	}
--- trunk/__v3/aCMS/Routes.php
+++ trunk/__v3/aCMS/Routes.php
@@ -1,5 +1,6 @@
 <?php
 	return array(
-		'/:url_str/' => 'Page'
+		'/'				=> 'Home', 
+		'/:url_str/'	=> 'Page'
 	);
 ?>
