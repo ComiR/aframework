c7b0eaec35b2d6afd4eae8f437a06452f474571c
Andreas Lagerkvist <powerbuoy@gmail.com>
2008-08-14 17:49:18 +0000

    Fixed a potential bug (reset exclude in js/css-comp) in code-compression and tidied up code a lil

--- __v3/aFramework/Modules/JSCompressor/JSCompressorModule.php
+++ __v3/aFramework/Modules/JSCompressor/JSCompressorModule.php
@@ -1,11 +1,9 @@
 <?php
 	class aFramework_JSCompressorModule extends aFramework_CodeCompressorModule {
-		public static $tplVars = array();
-		public static $tplFile = true;
-		public static $forceController = false;
-
 		public static function run() {
 			self::$type = 'js';
+			self::$exclude = array();
+
 			parent::run();
 		}
 	}
