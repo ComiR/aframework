9f84a7a682b2f13230cfd8ba76f106100508ba3a
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2008-08-14 23:39:32 +0000

    Made aFramework complete enough that it can run by itself (routes, styles and config)

---
+++ trunk/__v3/aFramework/Config.php
@@ -0,0 +1,24 @@
+<?php
+	/**
+	 * Site specific config
+	 **/
+	define('DB_USER',			'root');
+	define('DB_PASS',			'');
+	define('DB_HOST',			'localhost');
+	define('DB_NAME',			'afv3');
+	define('DB_TABLE_PREFIX',	'');
+
+	define('ALLOW_STYLES',		true);
+	define('DEFAULT_STYLE',		'default');
+
+	define('SITE_AUTHOR',		'Andreas Lagerkvist');
+	define('SITE_TITLE',		'aFramework');
+	define('SITE_TAGLINE',		'You shouldn\'t be running just aFramework');
+
+	define('ADMIN_USER',		'admin');
+	define('ADMIN_PASS',		'nimda');
+
+	define('SITE_HIERARCHY',	CURRENT_SITE);
+
+	define('GA_ID',				false); # Google analytics ID
+?>
