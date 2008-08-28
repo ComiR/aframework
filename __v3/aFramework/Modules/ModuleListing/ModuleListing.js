6428eeb897e4db37152b0ef3e61e5718a8de8e87
Andreas Lagerkvist <powerbuoy@gmail.com>
2008-08-28 14:24:55 +0000

    modlistingwork also added debug that i missed before

--- __v3/aFramework/Modules/ModuleListing/ModuleListing.js
+++ __v3/aFramework/Modules/ModuleListing/ModuleListing.js
@@ -1,11 +1,13 @@
 var ModuleListing = {
 	init: function() {
+		// Only run if admin
 		if(!$('body.admin').length) {
 			this.makeModulesDraggable();
 			this.addRemoveButtonsToUsedModulesAndMakeDroppable();
 		}
 	}, 
 
+	// Makes modules in module-listing, as well as module-listing itself, draggable
 	makeModulesDraggable: function() {
 		$('#module-listing').draggable({
 			handle:			'h2'
@@ -19,43 +21,40 @@ var ModuleListing = {
 		});
 	}, 
 
+	// Adds a [remove]-button to every module currently in use on the page
+	// Also make them "droppable"
 	addRemoveButtonsToUsedModulesAndMakeDroppable: function() {
 		// Add remove-button to all modules in use and remove the module when you click it
 		var addRemoveButton = function(module, moduleName, controllerInUse) {
-			var remove = $('<button title="Remove module ' +moduleName +' from controller ' +controllerInUse +'">X</button>').appendTo(module);
+			var info	= 'Remove module ' +moduleName +' from controller ' +controllerInUse;
+			var remove	= $('<button title="' +info +'">X</button>').appendTo(module);
 
-			module.css({position: 'relative'});
-			remove.css({position: 'absolute', left: '0', top: '0'});
-
+			// When you click the remove-button
 			remove.click(function() {
-				var ajaxPostData = {
-					module_listing_remove_module:	1,
-					module_to_remove:				moduleName, 
-					controller_in_use:				controllerInUse
-				};
-
-			//	alert('Removing ' +ajaxPostData.module_to_remove +' from ' +ajaxPostData.controller_in_use);
-
 				// Remove the in-use class from the module in the module-list so user can re-add it
 				$('#mod-' +module.attr('id')).removeClass('in-use');
 
 				// See if module contained any children (look for remove-buttons), if so re-add them to list as-well
 				module.find('div button[title^="Remove module "]').each(function() {
-					$('#mod-' +$(this).parents('div').attr('id')).removeClass('in-use');
+					$('#mod-' +$(this).parents('div').eq(0).attr('id')).removeClass('in-use');
 				});
 
 				// Remove the module-div from the page
 				module.remove();
 
 				// Ajax the change of the controller
+				$.post('/?module=ModuleListing', {
+					module_listing_remove_module:	1,
+					module_to_remove:				moduleName, 
+					controller_in_use:				controllerInUse
+				});
 			});
 		};
 
-		// Make every module droppable so you can drag modules from the list in to other modules
+		// Make every module droppable so you can drag modules from the list into other modules
 		var makeDroppable = function(module, moduleName, controllerInUse) {
 			module.droppable({
 				accept:		'div[id]:not(#module-listing)', 
-				hoverClass:	'droppable-hover', 
 				tolerance:	'intersect', 
 				greedy:		true, 
 				// When a module is dragged over another
@@ -64,17 +63,13 @@ var ModuleListing = {
 					$('div.module-listing-ghost-module').remove();
 
 					// Get the position of the module
-					var targetOffset = ui.element.offset();
-					var ghostMod = '<div class="module-listing-ghost-module"><h2>' +ui.draggable.find('h3').text() +'</h2></div>';
+					var targetOffset	= ui.element.offset();
+					var ghostMod		= '<div class="module-listing-ghost-module"><h2>' +ui.draggable.find('h3').text() +'</h2></div>';
 
 					// If the module being dragged is above the module
 					if(ui.absolutePosition.top < targetOffset.top) {
 						ghostMod = $(ghostMod).insertBefore(module);
 						ghostMod.parent().addClass('droppable-over');
-					}
-					// If it's beneath
-					else if(false) {
-						ghostMod = $(ghostMod).insertAfter(module);
 					}
 					// It's inside
 					else {
@@ -89,36 +84,45 @@ var ModuleListing = {
 				}, 
 				// When dropping module
 				drop: function(ev, ui) {
-					var ajaxPostData = {
-						module_listing_add_module:	1, 
-						target:						moduleName, 
-						module_to_add:				ui.draggable.find('h3').text(), 
-						controller_in_use:			controllerInUse
-					};
+					var moduleToAdd		= ui.draggable.find('h3').text();
+					var moduleToAddID	= ui.draggable.attr('id').replace(/^mod-/, '');
+					var info			= 'Add ' +moduleToAdd +' to ' +moduleName +' in ' +controllerInUse;
 
-				//	alert('Adding ' +ajaxPostData.module_to_add +' to ' +ajaxPostData.target +' in ' +ajaxPostData.controller_in_use);
-
+					// Remove any potential droppable-over-class
 					$('.droppable-over').removeClass('droppable-over');
 
 					// Hide the newly added module from the module-list
 					ui.draggable.addClass('in-use').css({left: 0, top: 0});
 
 					// Use the ghost-div for the new module
-					var newMod = $('div.module-listing-ghost-module').attr('id', ui.draggable.attr('id').replace(/^mod-/, '')).append('<p>Loading...</p>');
+					var newMod = $('div.module-listing-ghost-module').attr('id', moduleToAddID).append('<p>Loading...</p>');
 
 					// Now fill the div with the module's stuff
-					$.get('/?module=' +ajaxPostData.module_to_add, function(data) {
+					$.get('/?module=' +moduleToAdd, function(data) {
 						newMod.html(data).removeClass('module-listing-ghost-module').addClass('module-listing-used-module');
-/*
-						if(typeof(aFramework.modules[ajaxPostData.module_to_add].run) == 'function') {
-							aFramework.modules[ajaxPostData.module_to_add].run();
-						}
-*/
-						addRemoveButton(newMod, ajaxPostData.module_to_add, controllerInUse);
-						makeDroppable(newMod, ajaxPostData.module_to_add, controllerInUse);
+
+					//	if(typeof(aFramework.modules[ajaxPostData.module_to_add].run) == 'function') {
+					//		aFramework.modules[ajaxPostData.module_to_add].run();
+					//	}
+
+						addRemoveButton(newMod, moduleToAdd, controllerInUse);
+						makeDroppable(newMod, moduleToAdd, controllerInUse);
 					});
 
 					// Ajax the change of the controller
+					var ajaxPostData = {
+						module_listing_add_module:	1, 
+						add_type:					'append', 
+						target:						moduleName, 
+						module_to_add:				moduleToAdd, 
+						controller_in_use:			controllerInUse
+					};
+
+					if(!ui.element.find('#' +moduleToAddID).length) {
+						ajaxPostData.add_type = 'before';
+					}
+
+					$.post('/?module=ModuleListing', ajaxPostData);
 				}
 			});
 		};
