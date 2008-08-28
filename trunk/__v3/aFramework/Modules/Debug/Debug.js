c205fe987641f1cc694b022ff287daab89d13f6e
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2008-08-28 14:24:55 +0000

    modlistingwork also added debug that i missed before

---
+++ trunk/__v3/aFramework/Modules/Debug/Debug.js
@@ -0,0 +1,71 @@
+Debug = {
+	run: function() {
+		this.addLinks('h2', function(a) {
+			var debug = document.getElementById('debug');
+			if(debug.className == 'hide') {
+				debug.className = '';
+			}
+			else {
+				debug.className = 'hide';
+			}
+		});
+
+		this.addLinks('h3', function(a) {
+			if(a.parentNode.className == 'hide') {
+				a.parentNode.className = '';
+			}
+			else {
+				a.parentNode.className = 'hide';
+			}
+		});
+
+		this.addLinks('h4', function(a) {
+			if(a.parentNode.parentNode.className == 'hide') {
+				a.parentNode.parentNode.className = '';
+			}
+			else {
+				a.parentNode.parentNode.className = 'hide';
+			}
+		});
+
+		this.addModuleHighlighting();
+	}, 
+
+	addLinks: function(el, oc) {
+		var div	= document.getElementById('debug');
+		var h	= div.getElementsByTagName(el);
+
+		for(var i = 0; h[i]; i++) {
+			var a			= document.createElement('a');
+
+			a.href			= '#';
+			a.onclick		= function() {oc(this); return false;};
+			a.innerHTML		= h[i].innerHTML;
+			h[i].innerHTML	= '';
+
+			h[i].appendChild(a);
+
+			oc(a);
+		}
+	}, 
+
+	addModuleHighlighting: function() {
+		var div	= document.getElementById('debug');
+		var lis = div.getElementsByTagName('li');
+
+		for(var i = 0; lis[i]; i++) {
+			if(document.getElementById(lis[i].title)) {
+				lis[i].onmouseover = function() {
+					document.getElementById(this.title).style.opacity = .2;
+				};
+				lis[i].onmouseout = function() {						
+					document.getElementById(this.title).style.opacity = 1;
+				};
+			}
+		}
+	}
+};
+
+if($('body.debug').length) {
+	Debug.run();
+}
---
+++ trunk/__v3/aFramework/Modules/Debug/DebugModule.php
@@ -0,0 +1,13 @@
+<?php
+	class aFramework_DebugModule {
+		public static $tplVars = array();
+		public static $tplFile = true;
+		public static $forceController = false;
+
+		public static function run() {
+			if(!(DEBUG and ADMIN)) {
+				self::$tplFile = false;
+			}
+		}
+	}
+?>
