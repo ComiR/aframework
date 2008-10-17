28fc62513607a4f23f80076e96c3609bacbe8a1c
Andreas Lagerkvist <powerbuoy@gmail.com>
2008-10-17 17:18:09 +0000

    ADded text-shadow jw-plug

---
+++ __v3/aFramework/Modules/Base/jquery.textShadow.js
@@ -0,0 +1,44 @@
+/***
+@title:
+Text Shadow
+
+@version:
+1.0
+
+@author:
+Andreas Lagerkvist
+
+@date:
+2008-10-17
+
+@url:
+http://andreaslagerkvist.com/jquery/text-shadow/
+
+@license:
+http://creativecommons.org/licenses/by/3.0/
+
+@copyright:
+2008 Andreas Lagerkvist (andreaslagerkvist.com)
+
+@requires:
+jquery, jquery.textShadow.css
+
+@does:
+Adds a shadow to text
+
+@howto:
+jQuery('h2').textShadow();
+
+@exampleHTML:
+<p>I should have a shadow.</p>
+
+@exampleJS:
+jQuery('#jquery-text-shadow-example p').textShadow();
+***/
+jQuery.fn.textShadow = function() {
+	return this.each(function() {
+		var el = $(this);
+		el.html('<span class="jquery-text-shadow-text">' +el.html() +'</span>').css('position', 'relative');
+		jQuery('<span class="jquery-text-shadow">' +el.text() +'</span>').appendTo(el);
+	});
+};
