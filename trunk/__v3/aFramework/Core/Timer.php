bf1ebc449433cf01c68251eda931cec7b40576a4
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2008-11-11 22:38:34 +0000

    Added simple timer, may use later

---
+++ trunk/__v3/aFramework/Core/Timer.php
@@ -0,0 +1,13 @@
+<?php
+	class Timer {
+		private static $timer;
+
+		public static function start() {
+			self::$timer = microtime(true);
+		}
+
+		public static function stop() {
+			return microtime(true) - self::$timer;
+		}
+	}
+?>
