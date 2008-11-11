3363ddb87edd7e5a482e1ba66d43fe548718128f
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2008-11-11 22:42:48 +0000

    Added Timer to footer and converted all Config-stuff

--- trunk/__v3/aFramework/Config.php
+++ trunk/__v3/aFramework/Config.php
@@ -1,19 +1,69 @@
 <?php
-	Config::set('db.user',						'root');
-	Config::set('db.pass',						'');
-	Config::set('db.host',						'localhost');
-	Config::set('db.name',						'afv3');
-	Config::set('db.table_prefix',				'');
+	# Database
+	Config::set('db',							array(
+													'title'			=> 'Database', 
+													'description'	=> 'Database settings such as username and password.'
+												));
+	Config::set('db.user',						array(
+													'title'			=> 'Username', 
+													'value'			=> 'root'
+												));
+	Config::set('db.pass',						array(
+													'title'			=> 'Password', 
+													'value'			=> ''
+												));
+	Config::set('db.host',						array(
+													'title'			=> 'Host', 
+													'value'			=> 'localhost'
+												));
+	Config::set('db.name',						array(
+													'title'			=> 'Database name', 
+													'value'			=> 'afv3'
+												));
+	Config::set('db.table_prefix',				array(
+													'title'			=> 'Database table prefix', 
+													'value'			=> ''
+												));
 
-	Config::set('aframework.allow_styles',		true);
+	# aFramework
+	Config::set('aframework',					array(
+													'title'			=> 'General', 
+													'description'	=> 'General settings used by various modules in aFramework.'
+												));
+	Config::set('aframework.allow_styles',		array(
+													'title'			=> 'Allow user style switching (1/0)', 
+													'value'			=> true
+												));
 	Config::set('aframework.default_style',		'default');
-
-	Config::set('aframework.site_author',		'Andreas Lagerkvist');
-	Config::set('aframework.site_title',		'aFramework 3');
-	Config::set('aframework.site_tagline',		'You shouldn\'t be running just aFramework.');
+	Config::set('aframework.site_author',		array(
+													'value'			=> 'Andreas Lagerkvist', 
+													'default_value'	=> 'You'
+												));
+	Config::set('aframework.site_title',		array(
+													'value'			=> 'aFramework', 
+													'default_value'	=> 'My Awesome Site'
+												));
+	Config::set('aframework.site_tagline',		array(
+													'value'			=> 'You shouldn\'t be running just aFramework', 
+													'default_value'	=> 'My space on the web'
+												));
 	Config::set('aframework.contact_email',		'you@yourdomain.com');
-	Config::set('aframework.ga_id',				false);
+	Config::set('aframework.ga_id',				array(
+													'title'			=> 'Google analytics ID (if any)', 
+													'value'			=> false
+												));
 
-	Config::set('admin.user',					'admin');
-	Config::set('admin.pass',					'1234');
+	# Admin
+	Config::set('admin',						array(
+													'title'			=> 'Administration', 
+													'description'	=> 'Logged in as admin you can customize the page you\'re at as well as administrate your site\'s content.'
+												));
+	Config::set('admin.user',					array(
+													'title'			=> 'Username', 
+													'value'			=> 'admin'
+												));
+	Config::set('admin.pass',					array(
+													'title'			=> 'Password', 
+													'value'			=> '1234'
+												));
 ?>
--- trunk/__v3/aFramework/Core/Config.php
+++ trunk/__v3/aFramework/Core/Config.php
@@ -3,6 +3,15 @@
 		private static $config = array();
 
 		public static function set($k, $v) {
+			if(false === strpos($k, '.')) {
+				self::$config[$k]['__info'] = array_merge((array)self::$config[$k]['__info'], array(
+					'name'			=> $k, 
+					'title'			=> (is_array($v) and isset($v['title'])) ? htmlentities($v['title']) : htmlentities(ucfirst(str_replace('_', ' ', $v))), 
+					'description'	=> (is_array($v) and isset($v['description'])) ? htmlentities($v['description']) : htmlentities(ucfirst(str_replace('_', ' ', $v)))
+				));
+				return;
+			}
+
 			$levels	= explode('.', $k);
 			$tmp	= &self::$config;
 
@@ -11,6 +20,7 @@
 			}
 
 			$tmp = array(
+				'name'			=> $k, 
 				'value'			=> is_array($v) ? $v['value'] : $v, 
 				'key'			=> $k, 
 				'highest_key'	=> $l, 
@@ -31,7 +41,21 @@
 		}
 
 		public static function asArray() {
-			return self::$config;
+			$items		= array();
+			$configs	= array();
+			foreach(self::$config as $k => $v) {
+				foreach(self::$config[$k] as $ik => $iv) {
+					if('__info' != $ik) {
+						$items[$ik] = self::$config[$k][$ik];
+					}
+				}
+				$configs[] = array(
+					'info'	=> self::$config[$k]['__info'], 
+					'items'	=> $items
+				);
+				$items = array();
+			}
+			return $configs;
 		}
 	}
 ?>
