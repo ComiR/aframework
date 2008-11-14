b5cd9d15f0984e2221f7f37e51c88244e1c16740
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2008-11-14 00:57:43 +0000

    Added a config for aBlog

--- trunk/__v3/aBlog/Config.php
+++ trunk/__v3/aBlog/Config.php
@@ -11,4 +11,8 @@
 													'value'			=> 8, 
 													'title'			=> 'Number of recent articles on first page'
 												));
+	Config::set('ablog.comment_spam_time',		array(
+													'value'			=> 5, 
+													'title'			=> 'Minutes users need to wait between comments'
+												));
 ?>
