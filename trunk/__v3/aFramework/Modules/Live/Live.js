fde155eb22ab04b2ace24568aa72a427977683bd
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2008-11-16 20:15:18 +0000

    Added Live-module

---
+++ trunk/__v3/aFramework/Modules/Live/Live.js
@@ -0,0 +1,22 @@
+aFrameWork.modules.Live = {
+	run: function() {
+		this.doToggleLink();
+	}, 
+
+	doToggleLink: function() {
+		var show = 'Show live cam';
+		var hide = 'Hide live cam';
+		var live = $('#live object');
+
+		$('<p><a href="#" id="live-toggler">' +show +'</a></p>').insertAfter('#live h2');
+		live.hide();
+
+		$('#live-toggler').toggle(function() {
+			$(this).text(hide);
+			live.show();
+		}, function() {
+			$(this).text(show);
+			live.hide();
+		});
+	}
+};
