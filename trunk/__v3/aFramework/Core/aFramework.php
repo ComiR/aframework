2bd00c5aaad81ecdb5ae9558b9b164df47710d57
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2008-11-16 19:08:35 +0000

    y una mas

--- trunk/__v3/aFramework/Core/aFramework.php
+++ trunk/__v3/aFramework/Core/aFramework.php
@@ -258,9 +258,9 @@
 			# If $child_modules is set and $all doesn't contain
 			# it that means none of the module's template echo:ed
 			# $child_modules, append them autoamtically
-		#	if(isset($tplVarsAdd['child_modules']) and !empty($tplVarsAdd['child_modules']) and false === strpos($all, $tplVarsAdd['child_modules'])) {
-		#		$all .= $tplVarsAdd['child_modules'];
-		#	}
+			if(isset($tplVarsAdd['child_modules']) and !empty($tplVarsAdd['child_modules']) and false === strpos($all, $tplVarsAdd['child_modules'])) {
+				$all .= $tplVarsAdd['child_modules'];
+			}
 
 			return $all == '' ? false : $all;
 		}
