6ae9c79e53f8159cb95bc6cd58fd8bc9c096b9cf
Andreas Lagerkvist <powerbuoy@gmail.com>
2008-11-16 22:39:38 +0000

    Removed jQplugSite, added AL

---
+++ __v3/AndreasLagerkvist/Config.php
@@ -0,0 +1,18 @@
+<?php
+	Config::set('db.user',						'root');
+	Config::set('db.pass',						'root');
+	Config::set('db.host',						'localhost');
+	Config::set('db.name',						'afv3');
+
+	Config::set('general.default_style',		'default');
+	Config::set('general.site_author',			'Andreas Lagerkvist');
+	Config::set('general.site_title',			'AndreasLagerkvist.com');
+	Config::set('general.site_tagline',			'jQuery plug-ins and other web-related stuff');
+	Config::set('general.contact_email',		'youwish@gmail.com');
+	Config::set('general.ga_id',				false);
+
+	Config::set('admin.user',					'admin');
+	Config::set('admin.pass',					'1234');
+
+	Config::set('alcom.num_random_plugins',		3);
+?>
