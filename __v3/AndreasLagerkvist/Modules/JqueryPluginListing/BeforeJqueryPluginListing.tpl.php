688b924ae82ea225aadd98bd5b8328c48a5fdadb
Andreas Lagerkvist <powerbuoy@gmail.com>
2008-11-17 00:43:03 +0000

    Fix

--- __v3/AndreasLagerkvist/Modules/JqueryPluginListing/BeforeJqueryPluginListing.tpl.php
+++ __v3/AndreasLagerkvist/Modules/JqueryPluginListing/BeforeJqueryPluginListing.tpl.php
@@ -1,1 +1,7 @@
-<h2>jQuery Plug-ins</h2>
+<h2>My jQuery Plug-ins</h2>
+
+<p>Below you'll find a range of different plug-ins for my favourite JS-lib, jQuery.</p>
+
+<p>All the plug-ins are compatible with FF2+, IE7+, Safari 2+, Opera 9+ and Chrome 1+, they have all been validated against <a href="http://jslint.com">JSLint</a> and can stand compression, none of them use any global variables and all of them use 'jQuery' rather than '$'.</p>
+
+<p>There are <?php echo count($plugins); ?> plug-ins to check out at the moment, due to the heavy comment-chunk in the beginning of every file the filesize may be a bit intimidating. Selecting the minified version, however, normally reduces filesize by more than 50%.</p>
