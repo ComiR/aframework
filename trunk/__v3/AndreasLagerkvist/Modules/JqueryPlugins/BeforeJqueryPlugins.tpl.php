f32dfad2d0723a1357fabd25735e61bec5b64675
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-01-12 19:03:52 +0000

    Changed intro text, messed about with constants and changed one little character in center

--- trunk/__v3/AndreasLagerkvist/Modules/JqueryPlugins/BeforeJqueryPlugins.tpl.php
+++ trunk/__v3/AndreasLagerkvist/Modules/JqueryPlugins/BeforeJqueryPlugins.tpl.php
@@ -4,4 +4,4 @@
 
 <p>All the plug-ins are compatible with FF2+, IE7+, Safari 2+, Opera 9+ and Chrome 1+, they have all been validated against <a href="http://jslint.com">JSLint</a> and can stand compression, none of them use any global variables and all of them use 'jQuery' rather than '$'.</p>
 
-<p>There are <?php echo count($plugins); ?> plug-ins to check out at the moment, due to the heavy comment-chunk in the beginning of every file the filesize may be a bit intimidating. Selecting the minified version, however, normally reduces filesize by more than 50%.</p>
+<p>There are <?php echo count($plugins); ?> plug-ins to check out at the moment, due to the heavy comment-chunk in the beginning of every file the filesize may be a bit intimidating. Selecting the minified version, however, normally reduces filesize by more than 50%. In fact, only Image Zoom and Image Box are over 100 lines of code and most of them are under 50.</p>
