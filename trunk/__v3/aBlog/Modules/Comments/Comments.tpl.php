e70e94523baa371c168676d8bd8438541a181e04
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-06-24 20:07:07 +0000

    Link name to website, date to comment

--- trunk/__v3/aBlog/Modules/Comments/Comments.tpl.php
+++ trunk/__v3/aBlog/Modules/Comments/Comments.tpl.php
@@ -4,15 +4,21 @@
 			<li id="comment-<?php echo $c['comments_id']; ?>">
 				<h4>
 					<img src="http://www.gravatar.com/avatar.php?gravatar_id=<?php echo $c['email_md5']; ?>" alt="" /> 
-					<a href="<?php echo Router::urlFor('Article', $c); ?>#comment-<?php echo $c['comments_id']; ?>">
+					<?php if($c['website']) { ?>
+						<a href="<?php echo $c['website']; ?>">
+							<?php echo htmlentities($c['author']); ?>
+						</a>
+					<?php } else { ?>
 						<?php echo htmlentities($c['author']); ?>
-					</a>
+					<?php } ?>
 				</h4>
 
 				<p>
 					<small>
-						<?php echo Lang::get('Published'); ?> 
-						<?php echo date(Config::get('general.date_format'), strtotime($c['pub_date'])); ?>
+						<a href="<?php echo Router::urlFor('Article', $c); ?>#comment-<?php echo $c['comments_id']; ?>">
+							<?php echo Lang::get('Published'); ?> 
+							<?php echo date(Config::get('general.date_format'), strtotime($c['pub_date'])); ?>
+						</a>
 					</small>
 				</p>
 
