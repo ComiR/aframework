73814f45804f0a9c477f4a69df70241882c04c34
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-09-02 23:07:07 +0000

    Removed old common_gfx dir and made CodeCompressor support Module-based gfx/-dirs

--- __v3/aFramework/Modules/CodeCompressor/CodeCompressorModule.php
+++ __v3/aFramework/Modules/CodeCompressor/CodeCompressorModule.php
@@ -73,15 +73,15 @@
 		 */
 		private static function getLastModifiedFile ($style) {
 			$sites				= explode(' ', SITE_HIERARCHY);
-			$fileLastModified	= 0;
 			$dirs				= array();
 
+			# Create array of directories to look for files in
+			# Every sites every module and style can contain files
 			foreach ($sites as $site) {
 				$dirs[] = DOCROOT . $site . '/Styles/' . $style . '/';
-				$dirs[] = DOCROOT . $site . '/Styles/' . $style . '/controllers/';
-				$dirs[] = DOCROOT . $site . '/Styles/' . $style . '/modules/';
 				$path	= DOCROOT . $site . '/Modules/';
 
+				# Get module dirs
 				if (is_dir($path)) {
 					$dh = opendir($path);
 
@@ -93,13 +93,16 @@
 				}
 			}
 
-			foreach ($dirs as $dir) {
-				$flm = 0;
+			$flm = 0;
 
+			# Loop through all the direcoties
+			foreach ($dirs as $dir) {
 				if (is_dir($dir)) {
 					$dh = opendir($dir);
 
+					# Loop through all the files
 					while ($f = readdir($dh)) {
+						# Check if the file extention is what is being asked for (css/js)
 						if (self::$type == end(explode('.', $f))) {
 							$tmpFlm = filemtime($dir .$f);
 
@@ -109,13 +112,9 @@
 						}
 					}
 				}
-
-				if ($flm > $fileLastModified) {
-					$fileLastModified = $flm;
-				}
 			}
 
-			return $fileLastModified;
+			return $flm;
 		}
 
 		/**
@@ -128,30 +127,12 @@
 			$code	= '';
 
 			# We're starting backwards with the top-prio site first, 
-			# its code should be last in the generated style
+			# its code should be last in the generated style/script
 			foreach ($sites as $site) {
-				# CSS-"Controllers"-dir
-				$path = DOCROOT . $site . '/Styles/' . $style . '/controllers/';
-				if (is_dir($path)) {
-					$code = self::getCodeInDirs($path, '-page', $site, $style) . $code;
-				}
-
-				# CSS-"Modules"-dir
-				$path = DOCROOT . $site . '/Styles/' . $style . '/modules/';
-				if (is_dir($path)) {
-					$code = self::getCodeInDirs(array($path), '', $site, $style) . $code;
-				}
-
-				# CSS-"Root"-dir
+				# Style dir
 				$path = DOCROOT . $site . '/Styles/' . $style . '/';
 				if (is_dir($path)) {
-					$code = self::getCodeInDirs(array($path), false, $site, $style) . $code;
-				}
-
-				# Common-dir
-				$path = DOCROOT . $site . '/Styles/__common/';
-				if (is_dir($path)) {
-					$code = self::getCodeInDirs(array($path), false, $site, $style) . $code;
+					$code = self::getCodeInDirs(array($path), $site, $style) . $code;
 				}
 
 				# Module dirs
@@ -168,7 +149,7 @@
 					}
 				}
 
-				$code = self::getCodeInDirs($dirs, false, $site, $style) . $code;
+				$code = self::getCodeInDirs($dirs, $site, $style) . $code;
 			}
 
 			return $code;
@@ -179,10 +160,11 @@
 		 *
 		 * Gets all code in a particular directory. Sorts on filename
 		 */
-		private static function getCodeInDirs ($dirs, $prefixSelectorsWithFilename = false, $site, $style) {
+		private static function getCodeInDirs ($dirs, $site, $style) {
 			$code	= '';
 			$files	= array();
 
+			# Go through all the dirs and store the files of the requested type that are not prefixed with '__'
 			foreach ($dirs as $dir) {
 				$dh = opendir($dir);
 
@@ -195,22 +177,26 @@
 				}
 			}
 
+			# Sort them (that's why we keep the path in the 'key' and the lower-case path in the 'value')
 			asort($files);
 
+			# Now go through all the files and get the code
 			foreach ($files as $path => $name) {
-				$code .= "\n\n/* ==== [ $name ] ==== */\n";
 				$contents = file_get_contents($path);
-				$contents = str_replace('url(gfx/', 'url(' . WEBROOT . $site . '/Styles/' . $style . '/gfx/', $contents);
-				$contents = str_replace('url(common_gfx/', 'url(' . WEBROOT . 'aFramework/Styles/gfx/', $contents);
 
-				if (self::$type == 'css' and $prefixSelectorsWithFilename !== false) {
-					$fileNameNoExt = end(explode('/', substr($name, 0, -(strlen(end(explode('.', $name)))+1))));
-
-					$code .= CSSSelectorPrefixer::prefixSelectors($contents, '#' . $fileNameNoExt . $prefixSelectorsWithFilename);
+				# Authors can reference gfx/ from their CSS which will point to the current style's gfx-dir _or_
+				# if the file is located in a module-directory we point it to the module's gfx-dir
+				if (preg_match('/Modules\/(.*?)\//', $path, $matches)) {
+					$contents = str_replace('url(gfx/', 'url(' . WEBROOT . $site . '/Modules/' . $matches[1] . '/gfx/', $contents);
 				}
 				else {
-					$code .= $contents . "\n";
+					$contents = str_replace('url(gfx/', 'url(' . WEBROOT . $site . '/Styles/' . $style . '/gfx/', $contents);
 				}
+
+				# Authors can reference common_gfx/ from their CSS which we now point to the absolute path of the Base/gfx-dir
+				$contents = str_replace('url(common_gfx/', 'url(' . WEBROOT . 'aFramework/Modules/Base/gfx/', $contents);
+
+				$code .= $contents . "\n";
 			}
 
 			return $code;
