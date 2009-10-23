184345f73a7fcf386f17b8b7701392e996595788
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-10-23 08:55:10 +0000

    Do translation in template, not module

--- trunk/__v3/aFramework/Modules/SkipNavigation/SkipNavigation.tpl.php
+++ trunk/__v3/aFramework/Modules/SkipNavigation/SkipNavigation.tpl.php
@@ -2,8 +2,7 @@
 	<?php foreach($links as $l) { ?>
 		<li>
 			<a href="<?php echo $l['url']; ?>">
-				<?php echo Lang::get('Skip to'); ?> 
-				<?php echo $l['title']; ?>
+				<?php echo Lang::get('Skip to ' . $l['title']); ?>
 			</a>
 		</li>
 	<?php } ?>
