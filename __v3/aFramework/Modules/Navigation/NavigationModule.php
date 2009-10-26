43ef20eb4bf766a651225c6d6d5b1995f2f9e7bf
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-10-26 06:08:26 +0000

    Moved NavigationModule to aFramework and refactored

---
+++ __v3/aFramework/Modules/Navigation/NavigationModule.php
@@ -0,0 +1,55 @@
+<?php
+	class aFramework_NavigationModule {
+		public static $tplVars = array();
+		public static $tplFile = true;
+
+		public static function run () {
+			self::addItem(array(
+				'title'	=> Lang::get('Home'), 
+				'url'	=> Router::urlFor('Home')
+			));
+
+			self::setSelectedNavigationItem();
+		}
+
+		public static function addItem ($arrItem, $pos = false) {
+			if ($pos === false) {
+				self::$tplVars['nav_items'][] = $arrItem;
+			}
+			else {
+				array_insert($arrItem, $pos, self::$tplVars['nav_items']);
+			}
+		}
+
+		public static function removeItem ($pos) {
+			array_splice(self::$tplVars['nav_items'], $pos, 1);
+		}
+
+		public function setSelectedNavigationItem () {
+			$url			= $_SERVER['REQUEST_URI'];
+			$tmpSelected	= array('url' => '', 'selected' => false);
+			$numItems		= count(self::$tplVars['nav_items']);
+
+			for ($i = 0; $i < $numItems; $i++) {
+				self::$tplVars['nav_items'][$i]['selected'] = false;
+
+				if (strpos($url, self::$tplVars['nav_items'][$i]['url']) === 0 and strlen($tmpSelected['url']) < strlen(self::$tplVars['nav_items'][$i]['url'])) {
+					$tmpSelected['selected'] = false;
+
+					self::$tplVars['nav_items'][$i]['selected'] = true;
+
+					$tmpSelected = &self::$tplVars['nav_items'][$i];
+				}
+			}
+
+			# If home is selected and we're not on the home-page, deselect it
+			$homeURL = Router::urlFor('Home');
+
+			for ($i = 0; $i < $numItems; $i++) {
+				if (self::$tplVars['nav_items'][$i]['selected'] and self::$tplVars['nav_items'][$i]['url'] == $homeURL and $url != $homeURL) {
+					self::$tplVars['nav_items'][$i]['selected'] = false;
+				}
+			}
+		}
+	}
+?>
