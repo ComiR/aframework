b0662999c1c8c6b6bd4d4187d7811435a3051147
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-10-27 14:56:24 +0000

    Navigation-fixes + added style-switcher

---
+++ __v3/aFramework/Modules/StyleSwitcher/StyleSwitcher.tpl.php
@@ -0,0 +1,14 @@
+<form method="get" action="">
+
+	<p>
+		<label>
+			Select Style<br/>
+			<select name="style">
+				<option value="default">aFramework Default Style</option>
+				<option value="clean">aFramework Clean Style</option>
+				<option value="grunge">aFramework Grunge Style</option>
+			</select>
+		</label> <input type="submit" value="Go"/>
+	</p>
+
+</form>
