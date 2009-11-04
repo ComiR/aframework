3bf7973b023e2bbd6e687b6a1e8e16d3b1fe6568
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-11-04 07:14:17 +0000

    Added Route for new Styles-controller

--- trunk/__v3/aFramework/Routes.php
+++ trunk/__v3/aFramework/Routes.php
@@ -3,6 +3,7 @@
 		'/'										=> 'Home', 
 		'/' . Lang::get('url.admin') . '/'		=> 'AdminLogin', 
 		'/' . Lang::get('url.contact') . '/'	=> 'Contact', 
-		'/' . Lang::get('url.search') . '/'		=> 'SearchResults'
+		'/' . Lang::get('url.search') . '/'		=> 'SearchResults', 
+		'/' . Lang::get('url.styles') . '/'		=> 'Styles'
 	);
 ?>
