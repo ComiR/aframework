03793b3bcd599d48098b521a34efd4e6b5414b3b
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-11-04 06:59:21 +0000

    DynAdmin-style, more Config and Styles-controller

--- __v3/aTestSite/Config.php
+++ __v3/aTestSite/Config.php
@@ -1,4 +1,14 @@
 <?php
 	Config::set('general.site_title',			'aTestSite');
 	Config::set('general.site_tagline',			'This is just an example aFramework site.');
+	Config::set('general.site_author',			'Some Dude');
+	Config::set('general.contact_email',		'your@email.com');
+	Config::set('general.default_style',		'clean');
+
+	Config::set('db.user',						'root');
+	Config::set('db.pass',						'root');
+	Config::set('db.host',						'localhost');
+
+	Config::set('admin.user',					'admin');
+	Config::set('admin.pass',					'1234');
 ?>
