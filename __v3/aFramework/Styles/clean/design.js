0ebb95f3f55e428e86829258547200a02b8074c1
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-11-04 07:08:05 +0000

    More work on clean design + thumbs + info

--- __v3/aFramework/Styles/clean/design.js
+++ __v3/aFramework/Styles/clean/design.js
@@ -1,4 +1,7 @@
 (function () {
+	//console.dir($('#style-switcher option'));
+
+	// Full-width bottom-bar
 	var distance = ($(document).width() - 960) / 2;
 
 	$('#tertiary-content, #footer').each(function () {
