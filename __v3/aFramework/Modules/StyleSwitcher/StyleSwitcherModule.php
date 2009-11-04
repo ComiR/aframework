3c9457d900ac059d7c6e5f8d188815092b1371f6
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-11-04 07:22:02 +0000

    Added Styles and StyleSwitcher modules

---
+++ __v3/aFramework/Modules/StyleSwitcher/StyleSwitcherModule.php
@@ -0,0 +1,10 @@
+<?php
+	class aFramework_StyleSwitcherModule {
+		public static $tplVars = array();
+		public static $tplFile = true;
+
+		public static function run () {
+			self::$tplVars['styles'] = Styles::get();
+		}
+	}
+?>
---
+++ __v3/aFramework/Modules/Styles/BeforeStyles.tpl.php
@@ -0,0 +1,7 @@
+<h2><?php echo Lang::get('Styles'); ?></h2>
+
+<p>
+	<?php echo Lang::get('There are currently'); ?> 
+	<?php echo count($styles); ?> 
+	<?php echo Lang::get('styles to choose from.'); ?>
+</p>
