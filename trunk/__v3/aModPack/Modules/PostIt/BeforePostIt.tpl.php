d42a69d155195150b2644056dbaab3f6d9a16bf3
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-11-12 05:41:51 +0000

    Added aModPack with a couple of module-ideas

---
+++ trunk/__v3/aModPack/Modules/PostIt/BeforePostIt.tpl.php
@@ -0,0 +1,10 @@
+<h2><?php echo Lang::get('Post It'); ?></h2>
+
+<p>
+	<strong>
+		<?php echo Lang::get('From'); ?> 
+		<?php echo $first_date; ?> 
+		<?php echo Lang::get('to'); ?> 
+		<?php echo $last_date; ?>.
+	</strong>
+</p>
---
+++ trunk/__v3/aModPack/Modules/PostIt/PostItModule.php
@@ -0,0 +1,26 @@
+<?php
+	class aModPack_PostItModule {
+		public static $tplVars = array();
+		public static $tplFile = true;
+
+		public static function run () {
+			self::$tplVars['post_its'] = array(
+				array(
+					'post_its_id'	=> 1,
+					'content'		=> 'Lorem ipsum dolor'
+				), 
+				array(
+					'post_its_id'	=> 2,
+					'content'		=> 'Dolor sit amet'
+				), 
+				array(
+					'post_its_id'	=> 3,
+					'content'		=> 'Consequeteur lipsumus dolirus'
+				)
+			);
+
+			self::$tplVars['first_date'] = '24th Oct';
+			self::$tplVars['last_date'] = '12th Nov';
+		}
+	}
+?>
