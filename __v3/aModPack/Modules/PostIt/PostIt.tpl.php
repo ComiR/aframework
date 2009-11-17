0c29d0859a9607fd117d60a01233a531eeeabe4d
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-11-17 06:58:13 +0000

    Removed post-it-in-page-admin

--- __v3/aModPack/Modules/PostIt/PostIt.tpl.php
+++ __v3/aModPack/Modules/PostIt/PostIt.tpl.php
@@ -2,16 +2,6 @@
 	<?php foreach ($post_its as $post_it) { ?>
 		<li>
 			<?php echo $post_it['content']; ?>
-
-			<?php if (ADMIN) { ?>
-				<form method="post" action="">
-					<p>
-						<input type="hidden" name="post_it_delete" value="1" />
-						<input type="hidden" name="post_its_id" value="<?php echo $post_it['post_its_id']; ?>" />
-						<input type="submit" value="<?php echo Lang::get('Delete'); ?>" />
-					</p>
-				</form>
-			<?php } ?>
 		</li>
 	<?php } ?>
 </ol>
