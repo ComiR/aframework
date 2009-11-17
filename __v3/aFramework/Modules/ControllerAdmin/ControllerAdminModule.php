b9e983e445654a4024415e91ae1b91e003a874a9
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-11-17 07:11:10 +0000

    Controller admin started

---
+++ __v3/aFramework/Modules/ControllerAdmin/ControllerAdminModule.php
@@ -0,0 +1,17 @@
+<?php
+	class aFramework_ControllerAdminModule {
+		public static $tplVars = array();
+		public static $tplFile = true;
+
+		public static function run () {
+			self::$tplFile = ADMIN ? true : false;
+
+			if (isset($_GET['controller_admin'])) {
+				$_SESSION['controller_admin'] = true;
+			}
+			elseif (isset($_GET['no_controller_admin'])) {
+				unset($_SESSION['controller_admin']);
+			}
+		}
+	}
+?>
