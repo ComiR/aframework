8c3cedc4ed5298194f59ef9b09af6f78a200d279
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-11-26 08:35:33 +0000

    Added Langswitcher

---
+++ trunk/__v3/aFramework/Modules/LangSwitcher/LangSwitcherModule.php
@@ -0,0 +1,22 @@
+<?php
+	class aFramework_LangSwitcherModule {
+		public static $tplVars = array();
+		public static $tplFile = true;
+
+		public static function run () {
+			$langs = explode(',', Config::get('general.allowed_langs'));
+
+			if (1 == count($langs)) {
+				return self::$tplFile = false;
+			}
+
+			foreach ($langs as $lang) {
+				self::$tplVars['langs'][] = array(
+					'title'		=> Lang::lcToName($lang),
+					'lc'		=> $lang, 
+					'selected'	=> CURRENT_LANG == $lang ? true : false
+				);
+			}
+		}
+	}
+?>
