31ddd3827a19b360f849e46710f5944fcdf81646
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-11-26 08:37:41 +0000

    Added OurFutureEU

---
+++ trunk/__v3/OurFutureEU/Config.php
@@ -0,0 +1,22 @@
+<?php
+	Config::set('db.user',						'root');
+	Config::set('db.pass',						'root');
+	Config::set('db.host',						'localhost');
+	Config::set('db.name',						'ole2');
+
+	Config::set('general.allowed_langs',		'en,se');
+	Config::set('general.default_lang',			'en');
+	Config::set('general.default_style',		'open-ole');
+	Config::set('general.site_author',			'the Our Life as Elderly Staff');
+	Config::set('general.site_title',			'Our Life as Elderly');
+	Config::set('general.site_tagline',			'Working to improve life for the elderly');
+	Config::set('general.contact_email',		'info@ourfuture.eu');
+	Config::set('general.date_format',			'l jS \of F Y');
+	Config::set('general.ga_id',				false);
+
+	Config::set('general.ie_script_support',	false);
+	Config::set('general.ie_css_support',		true);
+
+	Config::set('admin.user',					'admin');
+	Config::set('admin.pass',					'1234');
+?>
---
+++ trunk/__v3/OurFutureEU/Modules/LatestComic/LatestComic.tpl.php
@@ -0,0 +1,8 @@
+<h2><?php echo Lang::get('The Latest Comic'); ?></h2>
+
+<p>
+	<a href="<?php echo $image['url']; ?>">
+		<img src="<?php echo $image['url']; ?>" alt=""/>
+	</a><br/>
+	<?php echo Lang::get('by'); ?> <a href="http://bonton.com">Hans Wikström</a>
+</p>
