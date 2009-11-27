20b41b421d13a9bee892e6a687d6525d678cfeea
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-11-27 07:35:20 +0000

    added flags to lang-switcher links

--- trunk/__v3/aFramework/Modules/LangSwitcher/LangSwitcher.tpl.php
+++ trunk/__v3/aFramework/Modules/LangSwitcher/LangSwitcher.tpl.php
@@ -3,10 +3,12 @@
 		<li>
 			<?php if ($lang['selected']) { ?>
 				<strong>
+					<img src="<?php echo Router::urlForFile('flags/' . $lang['lc'] . '.gif', 'aFramework'); ?>" alt=""/> 
 					<?php echo htmlentities($lang['title']); ?>
 				</strong>
 			<?php } else { ?>
 				<a href="<?php echo Router::urlForLang($lang['lc']); ?>">
+					<img src="<?php echo Router::urlForFile('flags/' . $lang['lc'] . '.gif', 'aFramework'); ?>" alt=""/> 
 					<?php echo htmlentities($lang['title']); ?>
 				</a>
 			<?php } ?>
