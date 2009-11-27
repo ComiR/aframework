391487c03b2ca8fa30121c30d8e728014c085878
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-11-27 07:29:32 +0000

    Added OLE Navigation, Sponsos and Partners modules and removed MainSponsors

---
+++ __v3/OurFutureEU/Modules/Partners/Partners.tpl.php
@@ -0,0 +1,44 @@
+<h2><?php echo Lang::get('Partners'); ?></h2>
+
+<ul>
+	<li>
+		<a href="http://www.lulea.se">
+			<img src="<?php echo Router::urlForFile('logos/lulea.jpg'); ?>" alt="City of Luleå"/>
+		</a>
+	</li>
+	<li>
+		<a href="http://www.ouka.fi">
+			<img src="<?php echo Router::urlForFile('logos/oulu.gif'); ?>" alt="City of Oulu"/>
+		</a>
+	</li>
+	<li>
+		<a href="http://www.hornafjordur.is">
+			<img src="<?php echo Router::urlForFile('logos/hornafjordur.gif'); ?>" alt="Hornafjordur"/>
+		</a>
+	</li>
+	<li>
+		<a href="http://www.bodo.kommune.no">
+			<img src="<?php echo Router::urlForFile('logos/bodo2.jpg'); ?>" alt="Municipality of Bodö"/>
+		</a>
+	</li>
+	<li>
+		<a href="http://www.fkf.fo">
+			<img src="<?php echo Router::urlForFile('logos/faroe.jpg'); ?>" alt="The Faroe association of municipalitys"/>
+		</a>
+	</li>
+	<li>
+		<a href="http://www.hafnarfjordur.is">
+			<img src="<?php echo Router::urlForFile('logos/hafnarfjordur.jpg'); ?>" alt="Hafnarfjarðarbær"/>
+		</a>
+	</li>
+	<li>
+		<a href="http://www.akureyri.is">
+			<img src="<?php echo Router::urlForFile('logos/akureyri.jpg'); ?>" alt="Akureyri"/>
+		</a>
+	</li>
+	<li>
+		<a href="http://www.naerverk.fo/">
+			<img src="<?php echo Router::urlForFile('logos/naerverkid.jpg'); ?>" alt="Nærverkid"/>
+		</a>
+	</li>
+</ul>
