3da8310823a189924710fdc1d8431a237b621451
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-12-01 05:52:30 +0000

    Hijaxed prev/next-links in calendar

---
+++ trunk/__v3/aBlog/Modules/ArticleCalendar/ArticleCalendar.js
@@ -0,0 +1,28 @@
+aFramework.modules.ArticleCalendar = {
+	run: function () {
+		this.hijaxPrevNextLinks();
+	}, 
+
+	hijaxPrevNextLinks: function () {
+		jQuery('#article-calendar ul a').click(function () {
+			var clicked = jQuery(this);
+			var oldText = clicked.text();
+			var year	= clicked.attr('href').substr(-8).substr(0, 4);
+			var month	= clicked.attr('href').substr(-3).substr(0, 2);
+
+			clicked.text(Lang.get('Loading') + '...');
+
+			jQuery.get(WEBROOT + '?module=ArticleCalendar&year=' + year + '&month=' + month, function (data) {
+				if (data == '') {
+					clicked.parent().html(oldText);
+				}
+				else {
+					jQuery('#article-calendar').html(data);
+					aFramework.modules.ArticleCalendar.run();
+				}
+			});
+
+			return false;
+		});
+	}
+};
