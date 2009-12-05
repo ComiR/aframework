66766e86c69c2605c51208fc132ee06f7071f256
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-12-05 13:27:31 +0000

    Buggy code for remembering users language and a non-change in Base

--- trunk/__v3/aFramework/Modules/LangSwitcher/LangSwitcher.tpl.php
+++ trunk/__v3/aFramework/Modules/LangSwitcher/LangSwitcher.tpl.php
@@ -7,7 +7,7 @@
 					<?php echo htmlentities($lang['title']); ?>
 				</strong>
 			<?php } else { ?>
-				<a href="<?php echo Router::urlForLang($lang['lc']); ?>">
+				<a href="<?php echo Router::urlForLang($lang['lc']); ?>?set_lang">
 					<img src="<?php echo Router::urlForFile('flags/' . $lang['cc'] . '.gif', 'aFramework'); ?>" alt=""/> 
 					<?php echo htmlentities($lang['title']); ?>
 				</a>
