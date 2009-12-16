3316f6bf63ab37b90b88b7d04e9c0117ce58e4b0
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-12-16 12:08:48 +0000

    Styling and listing of modules in controller admin

--- trunk/__v3/aFramework/Modules/ControllerAdmin/ControllerAdminModule.php
+++ trunk/__v3/aFramework/Modules/ControllerAdmin/ControllerAdminModule.php
@@ -1,8 +1,35 @@
 <?php
 	class aFramework_ControllerAdminModule {
-		public static $tplVars = array();
-		public static $tplFile = true;
+		public static $tplVars		= array();
+		public static $tplFile		= true;
 
+		private static $notModules	= array(
+			'.', 
+			'..', 
+			'.svn', 
+			'Base', 
+			'Debug',
+			'Captcha', 
+			'WeatherInfo', 
+			'CodeCompressor', 
+			'JSPacker', 
+			'ModuleAdmin', 
+			'LangGenerator', 
+			'AutoStyleSwitcher', 
+			'DynItem', 
+			'DynNav', 
+			'RecentDynItems', 
+			'DynTables', 
+			'DynItems', 
+			'#text', 
+			'#comment', 
+			'#cdata-section'
+		);
+
+		/**
+		 * run
+		 *
+		 **/
 		public static function run () {
 			self::$tplFile = ADMIN ? true : false;
 
@@ -12,6 +39,214 @@
 			elseif (isset($_GET['no_controller_admin'])) {
 				unset($_SESSION['controller_admin']);
 			}
+
+			# Someone wants to add a module to the controller
+			if (isset($_POST['module_admin_add_module'])) {
+				$insertBefore = $_POST['add_type'] == 'before' ? true : false;
+
+				if (!('Base' == $_POST['target'] and $insertBefore)) {
+					self::addModuleToController($_POST['module_to_add'], $_POST['target'], $insertBefore, $_POST['controller_in_use']);
+				}
+
+				if (!XHR) {
+					redirect('?added_module');
+				}
+			}
+
+			# Someone wants to remove a module from the controller
+			if (isset($_POST['module_admin_remove_module'])) {
+				self::removeModuleFromController($_POST['module_to_remove'], $_POST['controller_in_use']);
+
+				if (!XHR) {
+					redirect('?removed_module');
+				}
+			}
+
+			# Get all available modules for this site
+			self::$tplVars = self::getAvailableModules(isset(Router::$params['controller']) ? Router::$params['controller'] : 'Home');
 		}
+
+		/**
+		 * getAvailableModules
+		 *
+		 * Gets all available modules for this site (to be listed in module-listing)
+		 **/
+		private static function getAvailableModules($controller) {
+			$sites					= explode(' ', SITE_HIERARCHY);
+			$availableModules		= array();
+			$usedModules			= array();
+			$modulesInController	= self::getModulesInController($controller);
+
+			foreach ($sites as $site) {
+				$modulesDir = DOCROOT . $site . '/Modules/';
+
+				if (is_dir($modulesDir)) {
+					$dh = opendir($modulesDir);
+
+					while ($f = readdir($dh)) {
+						if (!in_array($f, self::$notModules) and is_dir($modulesDir . $f)) {
+							$newModule = array(
+								'name'		=> $f, 
+								'html_id'	=> strtolower(ccFix($f)), 
+								'thumb'		=> file_exists($modulesDir . $f . '/thumb.png') ? WEBROOT . $site . '/Modules/' . $f . '/thumb.png' : WEBROOT . 'aFramework/Files/icons/80x80-round-gradient.png'
+							);
+
+							if (array_key_exists($f, $modulesInController)) {
+								$availableModules[$f]			= $newModule;
+								$availableModules[$f]['in_use']	= true;
+							}
+							else {
+								$usedModules[$f]			= $newModule;
+								$usedModules[$f]['in_use']	= false;
+							}
+						}
+					}
+				}
+			}
+
+			sort($availableModules);
+		#	sort($usedModules);
+
+			$modules = array_merge($usedModules, $availableModules);
+
+			return array('available_modules' => $modules, 'modules_in_controller' => $modulesInController);
+		}
+
+		/**
+		 * getModulesInController
+		 *
+		 * Gets all modules in the currently used controller (so we can differentiate between used and unused modules)
+		 * These also include 
+		 **/
+		private static function getModulesInController ($controller) {
+			$path = self::getControllerPath($controller);
+
+			$doc = new DOMDocument();
+			$doc->load($path);
+
+			return self::__getModulesInController($doc->getElementsByTagName('Base'));
+		}
+
+		/**
+		 * __getModulesInController
+		 *
+		 **/
+		private static function __getModulesInController ($modules) {
+			$mods = array();
+
+			foreach ($modules as $mod) {
+				if (!in_array($mod->nodeName, self::$notModules)) {
+					$modName = $mod->nodeName == 'Wrapper' ? $mod->nodeName . ':' . $mod->getAttribute('name') : $mod->nodeName;
+					$mods[$modName] = array(
+						'name'		=> $modName, 
+						'html_id'	=> $mod->nodeName == 'Wrapper' ? $mod->getAttribute('name') : strtolower(ccFix($mod->nodeName))
+					);
+				}
+
+				if ($mod->hasChildNodes()) {
+					$mods = array_merge($mods, self::__getModulesInController($mod->childNodes));
+				}
+			}
+
+			return $mods;
+		}
+
+		/**
+		 * getControllerPath
+		 *
+		 **/
+		private static function getControllerPath ($controller) {
+			$sites = explode(' ', SITE_HIERARCHY);
+			$path = false;
+
+			foreach ($sites as $site) {
+				$path = DOCROOT . $site . '/Controllers/' . $controller . '.xml';
+
+				if (file_exists($path)) {
+					return $path;
+				}
+			}
+		}
+
+		/**
+		 * addModuleToController
+		 *
+		 **/
+		private static function addModuleToController ($module, $target, $insertBefore = false, $controller) {
+			# Load the controller
+			$path	= self::getControllerPath($controller);
+			$doc	= new DOMDocument();
+
+			$doc->load($path);
+
+			# Create the new node
+			$newModule = $doc->createElement($module);
+
+			# If target it's a wrapper-node, get name-attr
+			$wrapperName = false;
+
+			if ('Wrapper' == substr($target, 0, 7)) {
+				$wrapperInfo	= explode(':', $target);
+				$wrapperName	= $wrapperInfo[1];
+				$target			= $wrapperInfo[0];
+			}
+
+			$targetModules = $doc->getElementsByTagName($target);
+
+			foreach ($targetModules as $mod) {
+				# If target is a wrapper, check that the name is correct
+				if ($wrapperName) {
+					if ($wrapperName == $mod->getAttribute('name')) {
+						if ($insertBefore) {
+							$mod->parentNode->insertBefore($newModule, $mod); 
+						}
+						else {
+							$mod->appendChild($newModule); 
+						}
+						break;
+					}
+				}
+				# Otherwise just add at first occurence
+				else {
+					if ($insertBefore) {
+						$mod->parentNode->insertBefore($newModule, $mod); 
+					}
+					else {
+						$mod->appendChild($newModule); 
+					}
+					break;
+				}
+			}
+
+			file_put_contents($path, $doc->saveXML());
+
+			return true;
+		}
+
+		/**
+		 * removeModuleFromController
+		 *
+		 **/
+		private static function removeModuleFromController ($module, $controller) {
+			$path = self::getControllerPath($controller);
+
+			$doc = new DOMDocument();
+			$doc->load($path);
+
+			$modules = $doc->getElementsByTagName($module);
+			$mod = false;
+
+			foreach ($modules as $m) {
+				$mod = $m;
+				break;
+			}
+
+			$mod->parentNode->removeChild($mod);
+
+			file_put_contents($path, $doc->saveXML());
+
+			return true;
+		}
+
 	}
 ?>
