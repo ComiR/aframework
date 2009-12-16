ae9c93f4b62422ac0d466156932812485bcb8e6e
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-12-16 11:41:34 +0000

    New lang and fixes to Lang()-calls

--- __v3/aDynAdmin/Modules/DynItems/NoItems.tpl.php
+++ __v3/aDynAdmin/Modules/DynItems/NoItems.tpl.php
@@ -1,5 +1,5 @@
 <p>
-	<?php echo Lang::get('It appears you don\'t have any'); ?> 
+	<?php echo Lang::get("It appears you don't have any"); ?> 
 	<?php echo strtolower($table['title']); ?> 
 	<?php echo Lang::get('yet, why not'); ?> 
 	<a href="<?php echo Router::urlFor('AddDynItem', array('table_name' => $table['name'])); ?>">
