7b84b63d32c2222347d8306458bc78ae28aa999d
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-12-16 12:02:53 +0000

    Moved getLangsInDirs

--- trunk/__v3/aFramework/Modules/LangGenerator/LangGeneratorModule.php
+++ trunk/__v3/aFramework/Modules/LangGenerator/LangGeneratorModule.php
@@ -5,44 +5,7 @@
 
 		public static function run () {
 			$site					= isset($_GET['site']) ? $_GET['site'] : 'aFramework';
-			$path					= basename(DOCROOT . $site);
-			self::$tplVars['langs']	= self::getLangsInDir($path);
-		}
-
-		private static function getLangsInDir ($dir) {
-			$langs = array();
-
-			if (is_dir($dir)) {
-				$dh = opendir($dir);
-
-				while ($f = readdir($dh)) {
-					if ($f != '..' and $f != '.') {
-						if (is_dir($dir . '/' . $f)) {
-							$newLangs = self::getLangsInDir($dir . '/' . $f);
-							$langs = $newLangs !== false ? array_merge($langs, $newLangs) : $langs;
-						}
-						else {
-							$ext = end(explode('.', $f));
-
-							if (in_array($ext, array('js', 'php'))) {
-								$pattern	= '/Lang(::|\.)get\(\'(.*?)\'(.*?)\)/';
-								$matches	= array();
-								$contents	= file_get_contents($dir . '/' . $f);
-
-								if (preg_match_all($pattern, $contents, $matches)) {
-									foreach ($matches[2] as $key) {
-										$langs[$f][$key] = $key; # ucfirst(str_replace('_', ' ', $key));
-									}
-								}
-							}
-						}
-					}
-				}
-
-				return $langs;
-			}
-
-			return false;
+			self::$tplVars['langs']	= Lang::getLangsInDir(basename(DOCROOT . $site));
 		}
 	}
 ?>
