5cda5b893183ffce597f18c0caaff410ae530ec8
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-12-16 12:08:48 +0000

    Styling and listing of modules in controller admin

--- __v3/aFramework/Modules/ControllerAdmin/ControllerAdmin.tpl.php
+++ __v3/aFramework/Modules/ControllerAdmin/ControllerAdmin.tpl.php
@@ -3,3 +3,14 @@
 <?php } else { ?>
 	<p><a href="?controller_admin">Enable Controller Admin</a></p>
 <?php } ?>
+
+<?php if (CONTROLLER_ADMIN) { ?>
+	<ul>
+		<?php foreach ($available_modules as $module) { if (!$module['in_use']) { ?>
+			<li>
+				<img src="<?php echo $module['thumb']; ?>" alt=""/><br/>
+				<?php echo htmlentities(ccFix($module['name'], ' ')); ?>
+			</li>
+		<?php } } ?>
+	</ul>
+<?php } ?>
