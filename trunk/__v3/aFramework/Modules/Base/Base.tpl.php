5cc5f0a7e9f394d75808e36460ad124ec65719aa
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-12-17 08:42:05 +0000

    Set xml:lang aswell as lang, a little commenting in colourpicker and a new gradients-dir with one gradient

--- trunk/__v3/aFramework/Modules/Base/Base.tpl.php
+++ trunk/__v3/aFramework/Modules/Base/Base.tpl.php
@@ -1,7 +1,7 @@
 <?php /* <?xml version="1.0" encoding="utf-8"?> */ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="<?php echo CURRENT_LANG; ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo CURRENT_LANG; ?>" lang="<?php echo CURRENT_LANG; ?>">
 
 	<head>
 
