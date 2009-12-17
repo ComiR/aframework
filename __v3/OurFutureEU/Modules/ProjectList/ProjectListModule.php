aea6ffb38d14c15b9797ed0f3e58224924de175a
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-12-17 08:16:57 +0000

    Added ProjectsPage with ProjectList module, also added ProjectList module to ProjectPages

---
+++ __v3/OurFutureEU/Modules/ProjectList/ProjectListModule.php
@@ -0,0 +1,24 @@
+<?php
+	class OurFutureEU_ProjectListModule {
+		public static $tplVars = array();
+		public static $tplFile = true;
+
+		public static function run () {
+			$allPages		= Pages::get();
+			$projectPages	= array();
+
+			foreach ($allPages as $page) {
+				if (!$page['in_navigation'] and $page['url_str'] != 'home') {
+					$projectPages[] = $page;
+				}
+			}
+
+			if (count($projectPages)) {
+				self::$tplVars['projects'] = $projectPages;
+			}
+			else {
+				self::$tplFile = false;
+			}
+		}
+	}
+?>
