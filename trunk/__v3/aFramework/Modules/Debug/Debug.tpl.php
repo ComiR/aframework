1e4ccb16cd0a85b7247a02874c50eab636106887
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-12-17 08:27:33 +0000

    Debug styling and fixes

--- trunk/__v3/aFramework/Modules/Debug/Debug.tpl.php
+++ trunk/__v3/aFramework/Modules/Debug/Debug.tpl.php
@@ -11,7 +11,19 @@
 	</a>
 </p>
 
-<ul>
+<?php if (count($nav_items)) { ?>
+	<ul class="navigation">
+		<?php foreach ($nav_items as $item) { ?>
+			<li>
+				<a href="<?php echo $item['url']; ?>">
+					<?php echo escHTML($item['title']); ?>
+				</a>
+			</li>
+		<?php } ?>
+	</ul>
+<?php } ?>
+
+<ul class="debug">
 	<li>
 		<h3>Debug</h3>
 
@@ -55,16 +67,25 @@
 
 		<ul>
 			<?php foreach($tplVars['modules'] as $mod) { ?>
-				<li class="<?php echo $mod['html_id']; ?>">
-					<h4><?php echo $mod['class_name'] ? $mod['class_name'] : '[NoClass]' .'_' .$mod['name']; ?></h4>
+				<li>
+					<h4><?php echo $mod['classes'] ? $mod['classes'][0]['class_name'] : '[NoClass]' .'_' .$mod['name']; ?></h4>
 
 					<dl>
-						<dt>Module class</dt>
-						<dd><?php echo isset($mod['path']) ? str_replace(DOCROOT, '', $mod['path']) : '[no module-class]'; ?></dd>
-						<dt>Run time</dt>
-						<dd><?php echo $mod['run_time'] ? $mod['run_time'] : '[no run]'; ?> sec(s)</dd>
-						<dt>Number of queries</dt>
-						<dd><?php echo $mod['num_queries'] ? $mod['num_queries'] : '[none]'; ?></dd>
+						<dt>Module classes</dt>
+						<dd>
+							<?php if ($mod['classes']) { ?>
+								<ul>
+									<?php foreach ($mod['classes'] as $modClass) { ?>
+										<li>
+											<?php echo $modClass['class_name']; ?><br />
+											<small><?php echo $modClass['run_time']; ?> sec(s), <?php echo $modClass['num_queries']; ?> queries</small>
+										</li>
+									<?php } ?>
+								</ul>
+							<?php } else { ?>
+								[none]
+							<?php } ?>
+						</dd>
 						<dt>Template paths</dt>
 						<dd>
 							<?php if(count($mod['tpl_paths'])) { ?>
@@ -88,7 +109,7 @@
 											<?php 
 												if(is_array($v)) {
 													echo '<pre>';
-													htmlentitiesDebug($v);
+													varDumpEscHTML($v);
 													echo '</pre>';
 												}
 												else {
--- trunk/__v3/aFramework/Modules/Debug/DebugModule.php
+++ trunk/__v3/aFramework/Modules/Debug/DebugModule.php
@@ -6,5 +6,18 @@
 		public static function run () {
 			self::$tplFile = ADMIN ? true : false;
 		}
+
+		public static function addItem ($title, $url, $pos = false) {
+			if ($pos === false) {
+				self::$tplVars['nav_items'][] = array('title' => $title, 'url' => $url);
+			}
+			else {
+				if (!isset(self::$tplVars['nav_items'])) {
+					self::$tplVars['nav_items'] = array();
+				}
+
+				array_insert(array('title' => $title, 'url' => $url), $pos, self::$tplVars['nav_items']);
+			}
+		}
 	}
 ?>
