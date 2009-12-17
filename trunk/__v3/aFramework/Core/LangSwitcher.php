d1267d76e091eac3417da286b5cbd153b6c0a852
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-12-17 22:43:24 +0000

    Cleaned up lang-switcher a lil and switched to latest google analytics code

--- trunk/__v3/aFramework/Core/LangSwitcher.php
+++ trunk/__v3/aFramework/Core/LangSwitcher.php
@@ -1,6 +1,10 @@
 <?php
 	class LangSwitcher {
 		public static function run () {
+			self::setCurrentLang();
+		}
+
+		private static function setCurrentLang () {
 			$requestedURI	= isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/';
 			$allowedLangs	= explode(',', Config::get('general.allowed_langs'));
 			$currentLang	= Config::get('general.default_lang');
@@ -16,16 +20,15 @@
 					}
 
 					$currentLang = $requestedURIPortions[1];
-
-					# Remove the lang from the requestedURI so as not to affect routing
-					$requestedURI = str_replace('/' . $currentLang . '/', '/', $requestedURI);
 				}
 			}
 
 			define('CURRENT_LANG', $currentLang);
+		}
 
-			# Buggy with lang-free requests (like CodeCompressor...)
-		/*	$preferredLang = isset($_COOKIE['preferred_lang']) ? $_COOKIE['preferred_lang'] : CURRENT_LANG;
+		# Buggy with lang-free requests (like CodeCompressor...)
+		private static function rememberLang () {
+			$preferredLang = isset($_COOKIE['preferred_lang']) ? $_COOKIE['preferred_lang'] : CURRENT_LANG;
 
 			# Check if user has a preferred lang and that he's not trying to change it
 			if ($preferredLang != CURRENT_LANG and !isset($_GET['set_lang'])) {
@@ -39,7 +42,7 @@
 			# See if user is changing lang
 			elseif (isset($_GET['set_lang'])) {
 				setcookie('preferred_lang', CURRENT_LANG, time() + 31536000, WEBROOT);
-			}	*/
+			}
 		}
 	}
 ?>
