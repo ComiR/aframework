cc8e59f81378b512e5490645e082eab2f976b4c8
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-12-20 08:29:06 +0000

    Added QuickAbout-module and made meta-fields optional for Page

---
+++ __v3/aCMS/Modules/QuickAbout/QuickAboutAdmin.tpl.php
@@ -0,0 +1,15 @@
+<form method="post" action="">
+
+	<p>
+		<label>
+			<?php echo Lang::get('Short Description of'); ?> <?php echo escHTML(Config::get('general.site_title')); ?><br/>
+			<textarea name="content" rows="10" cols="60"><?php echo escHTML($content); ?></textarea>
+		</label>
+	</p>
+
+	<p>
+		<input type="hidden" name="quick_about_update" value="1"/>
+		<input type="submit" value="<?php echo Lang::get('Save Changes'); ?>"/>
+	</p>
+
+</form>
---
+++ __v3/aCMS/Modules/QuickAbout/QuickAboutModule.php
@@ -0,0 +1,37 @@
+<?php
+	class aCMS_QuickAboutModule {
+		public static $tplVars = array();
+		public static $tplFile = true;
+
+		public static function run () {
+			if (isset($_POST['quick_about_update'])) {
+				if (!($page = Pages::getPageByURLStr('__quickabout'))) {
+					Pages::insert(array(
+							'url_str'			=> '__quickabout', 
+							'pub_date'			=> date('Y-m-d H:i:s'), 
+							'in_navigation'		=> 0, 
+							'priority'			=> 0, 
+							'priority'			=> 0, 
+							'title'				=> 'Quick About', 
+							'content'			=> $_POST['content']
+						)
+					);
+
+					if (!XHR) {
+						redirect('?inserted_quick_about');
+					}
+				}
+				else {
+					Pages::update($page['pages_id'], array('content' => $_POST['content']));
+				}
+			}
+
+			if (!($page = Pages::getPageByURLStr('__quickabout'))) {
+				self::$tplVars['content'] = Lang::get("# About us\n\nNothing here yet.");
+			}
+			else {
+				self::$tplVars['content'] = $page['content'];
+			}
+		}
+	}
+?>
