2d2f67017d82370949e8ab693aac7919aa6a5c40
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-12-20 08:13:31 +0000

    CommentsModule: Hijaxed admin for spamming, hamming and deleteing

---
+++ trunk/__v3/aBlog/Modules/Comments/BeforeNoComments.tpl.php
@@ -0,0 +1,1 @@
+<h3><?php echo Lang::get('No Comments'); ?></h3>
--- trunk/__v3/aBlog/Modules/Comments/CommentsModule.php
+++ trunk/__v3/aBlog/Modules/Comments/CommentsModule.php
@@ -4,18 +4,46 @@
 		public static $tplFile = true;
 
 		public static function run () {
-			if (isset($_POST['comments_delete']) and ADMIN) {
-				self::deleteComment($_POST['comments_id']);
+			# Work out the article ID, GET on certain ajax-requests
+			if (isset($_GET['articles_id'])) {
+				$articlesID = $_GET['articles_id'];
+			}
+			# POST for when deleting all spams (on ajax too)
+			elseif (isset($_POST['articles_id'])) {
+				$articlesID = $_POST['articles_id'];
+			}
+			# Normal page-load, get it from the article-module
+			elseif (isset(aBlog_ArticleModule::$tplVars['article']['articles_id'])) {
+				$articlesID = aBlog_ArticleModule::$tplVars['article']['articles_id'];
 			}
 
-			if (isset(Router::$params['url_str'])) {
-				self::$tplVars['comments'] = Comments::getCommentsByArticleURLStr(Router::$params['url_str']);
-			}
-			elseif (isset($_GET['articles_id'])) {
-				self::$tplVars['comments'] = Comments::getCommentsByArticleID($_GET['articles_id']);
+			# Make sure we know which article we're on
+			if ($articlesID) {
+				self::$tplVars['articles_id'] = $articlesID;
 			}
 			else {
 				self::$tplFile = false;
+			}
+
+			# Handle delete, mark as spam and mark as ham
+			if (isset($_POST['comments_delete']) and ADMIN) {
+				self::deleteComment($_POST['comments_id']);
+			}
+			if (isset($_POST['comments_spam']) and ADMIN) {
+				self::spamComment($_POST['comments_id']);
+			}
+			if (isset($_POST['comments_ham']) and ADMIN) {
+				self::hamComment($_POST['comments_id']);
+			}
+			if (isset($_POST['comments_delete_all_spam'])) {
+				Comments::deleteAllSpamForArticle($articlesID);
+			}
+
+			# Grab the comments for this article
+			self::$tplVars['comments'] = Comments::getCommentsByArticleID($articlesID, ADMIN);
+
+			if (!self::$tplVars['comments']) {
+				self::$tplFile = 'NoComments';
 			}
 		}
 
@@ -24,6 +52,22 @@
 
 			if (!XHR) {
 				redirect('?deleted_comment');
+			}
+		}
+
+		private static function spamComment ($id) {
+			Comments::update($id, array('karma' => 0));
+
+			if (!XHR) {
+				redirect('?spammed_comment');
+			}
+		}
+
+		private static function hamComment ($id) {
+			Comments::update($id, array('karma' => 1));
+
+			if (!XHR) {
+				redirect('?hammed_comment');
 			}
 		}
 	}
