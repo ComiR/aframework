42de0c84af97a62c5f87c28de2a7c4597da526db
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-12-20 08:31:04 +0000

    Renamed style and made DynADmin language dependant

--- trunk/__v3/aDynAdmin/DBLib/DynItem.php
+++ trunk/__v3/aDynAdmin/DBLib/DynItem.php
@@ -11,6 +11,83 @@
 			return $cols;
 		}
 
+		public static function getFullTable ($tableName, $sort = '1', $order = 'ASC', $start = 0, $allowedLangs = false) {
+			$res = DB::qry('
+				SELECT 
+					* 
+				FROM 
+					' . escSQL($tableName) . ' 
+				ORDER BY 
+					' . escSQL($sort) . ' ' . escSQL($order) . ' 
+				LIMIT ' . escSQL($start) . ', ' . Config::get('adynadmin.num_items_per_page')
+			);
+
+			$i					= 0;
+			$tableNameNoLang	= $allowedLangs ? preg_replace('/^(' . implode('|', $allowedLangs) . ')_/', '', $tableName) : $tableName;
+			$table	= array(
+				'name'			=> $tableName, 
+				'title'			=> ucwords(str_replace('_', ' ', $tableNameNoLang)), 
+				'rows'			=> array(), 
+				'properties'	=> array()
+			);
+
+			while ($row = mysql_fetch_assoc($res)) {
+				if ($i++ == 0) {
+					foreach ($row as $property => $value) {
+						$table['properties'][] = array(
+							'name'	=> $property, 
+							'title'	=> ucwords(str_replace('_', ' ', $property))
+						);
+					}
+				}
+
+				$idColName = $allowedLangs ? preg_replace('/^(' . implode('|', $allowedLangs) . ')_/', '', $tableName) . '_id' : $tableName . '_id';
+
+				$table['rows'][] = array(
+					'id'			=> $row[$idColName], 
+					'properties'	=> $row
+				);
+			}
+
+			return $table;
+		}
+
+		public static function getTables ($translatedTables = false, $currentLang = false, $allowedLangs = false) {
+			# Get all tables
+			$res = DB::qry('SHOW TABLES');
+
+			while ($row = mysql_fetch_assoc($res)) {
+				$storeTable	= true;
+				$tableName	= end($row);
+
+				if ($translatedTables and $currentLang and $allowedLangs) {
+					# Remove potential lang-prefix from table name
+					$tableNameNoLang = preg_replace('/^(' . implode('|', $allowedLangs) . ')_/', '', $tableName);
+
+					# This is a translated table - remove language code
+					# and only store tables that are for the CURRENT_LANG
+					if (in_array($tableNameNoLang, $translatedTables)) {
+						$tableLang	= substr($tableName, 0, 2);
+
+						if ($tableLang != $currentLang) {
+							$storeTable = false;
+						}
+					}
+				}
+
+				if ($storeTable) {
+					$tables[] = array(
+						'name'			=> $tableName, 
+						'name_no_lang'	=> $tableNameNoLang, 
+						'title'			=> ucwords(str_replace('_', ' ', $tableNameNoLang)), 
+						'selected'		=> isset(Router::$params['table_name']) and Router::$params['table_name'] == $tableNameNoLang ? true : false
+					);
+				}
+			}
+
+			return $tables;
+		}
+
 		public static function getEmptyRow ($tableName) {
 			$cols	= self::getTableColumns($tableName);
 			$row	= array();
@@ -27,8 +104,9 @@
 			return $row;
 		}
 
-		public static function getByID ($tableName, $id) {
-			$res	= DB::qry('SELECT * FROM ' . escSQL($tableName) . ' WHERE ' . escSQL($tableName) . '_id = ' . escSQL($id) . ' LIMIT 1');
+		public static function getByID ($tableName, $id, $allowedLangs = false) {
+			$idCol	= $allowedLangs ? preg_replace('/^(' . implode('|', $allowedLangs) . ')_/', '', $tableName) . '_id' : $tableName . '_id';
+			$res	= DB::qry('SELECT * FROM ' . escSQL($tableName) . ' WHERE ' . escSQL($idCol) . ' = ' . escSQL($id) . ' LIMIT 1');
 			$row	= mysql_fetch_assoc($res);
 			$cols	= self::getTableColumns($tableName);
 			$nRow	= array();
