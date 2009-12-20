cedb1544768dd2f311418377e811c6932686942e
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-12-20 08:48:33 +0000

    Fix for remembering new visitor-data without having to reload page

--- trunk/__v3/aFramework/Core/VisitorData.php
+++ trunk/__v3/aFramework/Core/VisitorData.php
@@ -5,41 +5,48 @@
 	 * Stores and retrieves visitor data
 	 **/
 	final class VisitorData {
+		private static $data = array();
+
 		public static function run () {
+			# A form wants to remember its user
 			if (isset($_POST['remember_visitor_data']) and $_POST['remember_visitor_data']) {
 				self::setVisitorData($_POST);
+			}
+			# If not, set self::data if a cookie is set
+			elseif (isset($_COOKIE['visitor_data'])) {
+				$data = unserialize(stripslashes($_COOKIE['visitor_data']));
+				$data['remembered'] = true;
+
+				self::$data = $data;
 			}
 		}
 
 		public static function getVisitorData () {
-			$data = false;
-
-			if (isset($_COOKIE['visitor_data'])) {
-				$data = unserialize(stripslashes($_COOKIE['visitor_data']));
-				$data['remembered'] = true;
-			}
-
-			return $data;
+			return self::$data;
 		}
 
 		private static function setVisitorData ($data) {
 			$validData	= array('name', 'email', 'url', 'tel', 'author');
 			$convert	= array('author' => 'name', 'website' => 'url');
 			$oldData	= (array)self::getVisitorData();
-			$set		= array();
+			$newData	= array();
 
 			foreach ($data as $k => $v) {
 				if (in_array($k, $validData) || array_key_exists($k, $convert)) {
 					if (isset($convert[$k])) {
-						$set[$convert[$k]] = $v;
+						$newData[$convert[$k]] = $v;
 					}
 					else {
-						$set[$k] = $v;
+						$newData[$k] = $v;
 					}
 				}
 			}
 
-			setcookie('visitor_data', serialize(array_merge($oldData, $set)), time() + 31536000, WEBROOT);
+			self::$data = $latestData = array_merge($oldData, $newData);
+
+			setcookie('visitor_data', serialize($latestData), time() + 31536000, WEBROOT);
+
+			self::$data['remembered'] = true;
 		}
 	}
 ?>
