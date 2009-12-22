22db055a2cd52a937501bf3f257264cf63430c09
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-12-22 18:02:49 +0000

    More work on aFcom

--- trunk/__v3/aFrameworkCom/Modules/AframeworkUsers/AframeworkUsersModule.php
+++ trunk/__v3/aFrameworkCom/Modules/AframeworkUsers/AframeworkUsersModule.php
@@ -10,6 +10,12 @@
 		private static function getUsers () {
 			return array(
 				array(
+					'url'			=> 'http://ourfuture.eu', 
+					'title'			=> 'OurFuture.eu', 
+					'thumb'			=> WEBROOT . 'OurFutureEU/Styles/open-ole/thumb.png', 
+					'description'	=> 'We needed a multi-lingual, highly accessible website for our transnational project "Our Life as Elderly". aFramework was perfect for our needs.'
+				), 
+				array(
 					'url'			=> 'http://andreaslagerkvist.com', 
 					'title'			=> 'AndreasLagerkvist.com', 
 					'thumb'			=> WEBROOT . 'AndreasLagerkvist/Styles/darker/thumb.png', 
@@ -19,13 +25,13 @@
 					'url'			=> 'http://agnesekman.com', 
 					'title'			=> 'AgnesEkman.com', 
 					'thumb'			=> WEBROOT . 'AgnesEkman/Styles/cyber-a/thumb.png', 
-					'description'	=> 'Since I built Agnes\' site yet again I chose my home brewed framework :)'
+					'description'	=> 'As I also built Agnes\' site going with aFramework felt like the natural thing to do.'
 				), 
 				array(
-					'url'			=> 'http://ourfuture.eu', 
-					'title'			=> 'OurFuture.eu', 
-					'thumb'			=> WEBROOT . 'OurFutureEU/Styles/open-ole/thumb.png', 
-					'description'	=> 'We needed a multi-lingual, highly accessible website for our transnational project "Our Life as Elderly". aFramework was perfect for our needs.'
+					'url'			=> 'http://a-framework.org', 
+					'title'			=> 'a-Framework.org', 
+					'thumb'			=> WEBROOT . 'aFrameworkCom/Styles/aframework-com/thumb.png', 
+					'description'	=> 'How would it look if aFramework\'s official site _**didn\'t**_ run on aFramework? :)'
 				)
 			);
 		}
