86a21ebfe5a6f701e34d7d4b6b13cec1ea777e0c
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-12-22 11:25:38 +0000

    aFrameworkCom work + style thumbs

--- __v3/aFrameworkCom/Modules/AframeworkDownload/AframeworkDownload.tpl.php
+++ __v3/aFrameworkCom/Modules/AframeworkDownload/AframeworkDownload.tpl.php
@@ -1,14 +1,14 @@
 <ul>
 	<li>
+		<a href="http://demo.aframework.com">
+			<strong>Check out the Demo</strong><br/>
+			<small>Try before you <del>buy</del> <ins>download free</ins></small>
+		</a>
+	</li>
+	<li>
 		<a href="<?php echo $download['url']; ?>">
 			<strong>Download Latest</strong><br/>
 			<small><?php echo $download['version']; ?> - <?php echo round($download['filesize'] / 1024); ?> kb</small>
 		</a>
 	</li>
-	<!--<li>
-		<a href="http://demo.aframework.com">
-			<strong>Check out the Demo</strong><br/>
-			<small>Try before you <del>buy</del> <ins>download for free</ins></small>
-		</a>
-	</li>-->
 </ul>
