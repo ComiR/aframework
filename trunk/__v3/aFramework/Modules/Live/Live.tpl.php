c28a7d5042858d1c526ee04b24745922d9562563
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-12-23 16:17:07 +0000

    Worked on all sites controllers and removed "Live"

--- trunk/__v3/aFramework/Modules/Live/Live.tpl.php
+++
@@ -1,5 +0,0 @@
-<h2>Live!</h2>
-
-<object type="application/x-shockwave-flash" data="http://api.live.yahoo.com/swf/player/andreaslagerkvist" width="412" height="363">
-	<param name="movie" value="http://api.live.yahoo.com/swf/player/andreaslagerkvist" />
-</object>
