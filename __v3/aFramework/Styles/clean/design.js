78f34bc65d91d88f5692e7e686fb6ad056670590
Andreas Lagerkvist <powerbuoy@gmail.com>
2009-12-25 18:05:19 +0000

    More style work

--- __v3/aFramework/Styles/clean/design.js
+++
@@ -1,8 +0,0 @@
-(function () {
-	//console.dir($('#style-switcher option'));
-
-	// Full-width bottom-bar
-	var distance = ($(document).width() - 960) / 2;
-
-	$('#tertiary-content, #footer').fullPageWidthBar();
-})();
