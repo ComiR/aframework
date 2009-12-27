1c1847bcb93131827f8ffba6b95f1975c2257226
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2009-12-27 15:03:53 +0000

    LangGenerator can now generate other langs than en

--- trunk/__v3/aFramework/Utils/LangGenerator.php
+++ trunk/__v3/aFramework/Utils/LangGenerator.php
@@ -1,27 +1,36 @@
 <?php
+	header('Content-type: text/plain; charset=utf-8');
+
 	require_once '../Core/Lang.php';
 
 	if (isset($_GET['site'])) {
-		$site = $_GET['site'];
+		$site = basename($_GET['site']);
 	}
 	else {
 		die('Error: Please specify a site (?site=)');
 	}
 
-	$langs	= Lang::getLangsInDir('../../' . basename($site));
-	$code	= "<?php return array(\n";
+	$requestedLang		= isset($_GET['lang']) ? $_GET['lang'] : 'en';
+	$requestedPath		= "../../$site/Lang/$requestedLang.php";
+	$requestedExists	= file_exists($requestedPath);
+	$allLangs			= Lang::getLangsInDir('../../' . $site);
+	$requestedLang		= $requestedExists ? include $requestedPath : array();
+	$code				= "<?php return array(\n";
 
-	foreach ($langs as $site => $ls) {
-		$code .= "\n# $site\n";
+	foreach ($allLangs as $file => $strings) {
+		$code .= "\n# $file\n";
 
-		foreach ($ls as $k => $v) {
+		foreach ($strings as $k => $v) {
 			if (substr($v, 0, 4) == 'url.') {
 				$v = substr($v, 4);
 			}
+
+			# Translate to requested lang
+			$v = isset($requestedLang[$k]) ? $requestedLang[$k] : $v;
 
 			$code .= "'$k' => '$v', \n";
 		}
 	}
 
-	echo substr($code, 0, -3) . "\n\n); ?>"; 
+	echo substr($code, 0, -3) . "\n\n); ?>";
 ?>
