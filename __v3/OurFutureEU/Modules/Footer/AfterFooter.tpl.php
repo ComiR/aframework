9589d8fc7740987a2821e6a4cc00ce31be4e398b
Andreas Lagerkvist <powerbuoy@gmail.com>
2010-01-26 11:04:33 +0000

    OLE footer change + tiny ns-preview fix

--- __v3/OurFutureEU/Modules/Footer/AfterFooter.tpl.php
+++ __v3/OurFutureEU/Modules/Footer/AfterFooter.tpl.php
@@ -5,5 +5,5 @@
 	<a href="http://creativecommons.org/licenses/by/3.0/" title="<?php echo Lang::get('All contents are released under a CCv3'); ?>">
 		<?php echo Lang::get('Some Rights Reserved.'); ?>
 	</a> | 
-	Design and development by <a href="http://lagerkvist.eu">Lagerkvist &amp; Sons</a>
+	Design and development by <a href="http://lagerkvist.eu">Lagerkvist &amp; Söner AB</a>
 </p>
