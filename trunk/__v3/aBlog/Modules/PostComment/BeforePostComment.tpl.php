4e02ee05faca6b9376582a4b66c8e815d437173f
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2010-02-05 07:43:53 +0000

    Coding standards

--- trunk/__v3/aBlog/Modules/PostComment/BeforePostComment.tpl.php
+++ trunk/__v3/aBlog/Modules/PostComment/BeforePostComment.tpl.php
@@ -1,6 +1,6 @@
 <h3><?php echo Lang::get('Post a Comment'); ?></h3>
 
-<?php if ( $errors ) { ?>
+<?php if ($errors) { ?>
 	<p>
 		<strong>
 			<?php echo Lang::get('The form contains errors.'); ?> 
