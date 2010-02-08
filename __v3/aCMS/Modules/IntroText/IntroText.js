4e617d0fb2a6573fff2f63f30dde9ba6b045662a
Andreas Lagerkvist <powerbuoy@gmail.com>
2010-02-08 09:49:59 +0000

    Added useful IntroText module to aCMS

---
+++ __v3/aCMS/Modules/IntroText/IntroText.js
@@ -0,0 +1,9 @@
+aFramework.modules.IntroText = {
+	run: function () {
+		this.initMarkItUp();
+	}, 
+
+	initMarkItUp: function () {
+		$('#intro-text textarea[name=content]').markItUp(mySettings);
+	}
+};
