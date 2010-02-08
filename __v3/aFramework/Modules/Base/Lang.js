1e4319b4c7d349cb1ca8900bcf2f56cae01d55a0
Andreas Lagerkvist <powerbuoy@gmail.com>
2010-02-08 08:33:02 +0000

    Dont redeclare the lang-object in Lang as its now included AFTER the lang strings

--- __v3/aFramework/Modules/Base/Lang.js
+++ __v3/aFramework/Modules/Base/Lang.js
@@ -1,17 +1,14 @@
-var Lang = {
-	lang: {}, 
-	get: function (str) {
-		// If the string exists in the CURRENT_LANG use that
-		if (typeof(this.lang[CURRENT_LANG]) != 'undefined' && typeof(this.lang[CURRENT_LANG][str]) != 'undefined') {
-			return this.lang[CURRENT_LANG][str];
-		}
-		// Else, try default lang
-		else if (typeof(this.lang[DEFAULT_LANG]) != 'undefined' && typeof(this.lang[DEFAULT_LANG][str]) != 'undefined') {
-			return this.lang[DEFAULT_LANG][str];
-		}
-		// Else, just return the string
-		else {
-			return str;
-		}
+Lang.get = function (str) {
+	// If the string exists in the CURRENT_LANG use that
+	if (typeof(this.lang[CURRENT_LANG]) != 'undefined' && typeof(this.lang[CURRENT_LANG][str]) != 'undefined') {
+		return this.lang[CURRENT_LANG][str];
+	}
+	// Else, try default lang
+	else if (typeof(this.lang[DEFAULT_LANG]) != 'undefined' && typeof(this.lang[DEFAULT_LANG][str]) != 'undefined') {
+		return this.lang[DEFAULT_LANG][str];
+	}
+	// Else, just return the string
+	else {
+		return str;
 	}
 };
