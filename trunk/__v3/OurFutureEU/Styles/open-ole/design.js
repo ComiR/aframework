68488cf3a421817f6032ed0563b3ea5296613eb9
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2010-03-08 11:07:20 +0000

    Removed clean ole, added thumb to IE ole, removed shared files from open ole

--- trunk/__v3/OurFutureEU/Styles/open-ole/design.js
+++ trunk/__v3/OurFutureEU/Styles/open-ole/design.js
@@ -1,40 +1,11 @@
-OpenOLEStyle = {
+aFramework.styles.OpenOLE = {
 	run: function () {
 		this.fullWidthBottom();
-		this.documentsIcons();
 		this.movingSun();
-
-		window.onload = function () {
-			OpenOLEStyle.selfClearPositionAbsolute();
-			OpenOLEStyle.projectTabs();
-		};
 	}, 
 
 	fullWidthBottom: function () {
 		$('#tertiary-content, #footer').fullPageWidthBar();
-	}, 
-
-	documentsIcons: function () {
-		$('#documents tr').each(function (i) {
-			// Skip first row
-			if (i) {
-				var tr		= $(this);
-				var tds		= tr.find('td');
-				var link	= tds.eq(0).find('a');
-				var span	= tds.eq(1).wrapInner('<span/>').find('span');
-				var bgPos	= link.css('background-position').replace('0%', '100%');
-
-				span.css({
-					backgroundImage:	link.css('background-image'), 
-					backgroundPosition:	bgPos, 
-					backgroundRepeat:	link.css('background-repeat'), 
-					paddingTop:			link.css('padding-top'), 
-					paddingRight:		link.css('padding-left'), 
-					paddingBottom:		link.css('padding-bottom'), 
-					paddingLeft:		link.css('padding-right')
-				});
-			}
-		});
 	}, 
 
 	movingSun: function () {
@@ -54,31 +25,5 @@ OpenOLEStyle = {
 
 		updateSunPosition();
 		win.scroll(updateSunPosition);
-	}, 
-
-	selfClearPositionAbsolute: function () {
-		$('#contact-persons li').each(function () {
-			var minHeight = parseInt($(this).find('img').outerHeight(), 10) + 35;
-
-			$(this).css('min-height', minHeight + 'px');
-		});
-	}, 
-
-	projectTabs: function () {
-		if (document.getElementById('project-page-page')) {
-			// Add tab-list
-			$('<ul id="project-tabs"><li><a href="#page">' 
-				+ Lang.get('About') 
-				+ '</a></li><li><a href="#contact-persons">' 
-				+ Lang.get('Contact') 
-				+ '</a></li><li><a href="#documents">' 
-				+ Lang.get('Documents') 
-				+ '</a></li></ul>').insertAfter('#project-list').superSimpleTabs();
-
-			// Move the heading above the tabs
-			$('#page h2').prependTo('#primary-content');
-		}
 	}
 };
-
-OpenOLEStyle.run();
