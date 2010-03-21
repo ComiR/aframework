470cfddc1732c8839907130c81c2314016f5e691
Andreas Lagerkvist <powerbuoy@gmail.com>
2010-03-21 19:41:23 +0000

    bug t

--- __v3/aBugTracker/Modules/Projects/Projects.js
+++ __v3/aBugTracker/Modules/Projects/Projects.js
@@ -5,20 +5,35 @@ aFramework.modules.Projects = {
 
 	fisheyeMenu: function () {
 		// Build the required HTML
-		var fisheye = $('<div id="fisheye-projects" class="fisheye"><div class="fisheyeContainer"></div></div>').appendTo('#projects').find('div');
+		var fisheye = $('<div id="fisheye-projects"><div class="container"></div></div>')
+						.appendTo('#projects')
+						.find('div');
 
 		$('#projects li').each(function () {
-			var link	= $(this).find('a');
-			var text	= link.text();
-			var img		link.find('img').clone().attr('width', '30');
+			var li		= $(this);
+			var link	= li.find('a');
+			var text	= li.text();
+			var img		= link.find('img').clone();
 
 			$('<a href="' 
 				+ link.attr('href') 
-				+ '" class="fisheyeItem"><span>' 
+				+ '" class="item"><span>' 
 				+ text 
 				+ '</span></a>')
 					.append(img)
 					.appendTo(fisheye);
+		});
+
+		// Run fisheye
+		fisheye.parent().Fisheye({
+			maxWidth:	76,
+			items:		'a',
+			itemsText:	'span',
+			container:	'div.container',
+			items:		'a.item', 
+			itemWidth:	64,
+			proximity:	80,
+			halign:		'center'
 		});
 	}
 };
