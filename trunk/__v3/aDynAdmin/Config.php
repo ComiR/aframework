fd2f0e36b863409351839bb4da537616e25c53d3
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2010-03-25 11:01:18 +0000

    DynAdmin work (pagination + confirm delete etc)

--- trunk/__v3/aDynAdmin/Config.php
+++ trunk/__v3/aDynAdmin/Config.php
@@ -3,7 +3,7 @@
 													'title'			=> 'aDynAdmin', 
 													'description'	=> 'Settings for aFramework\'s dynamic admin; aDynAdmin'
 												));
-	Config::set('adynadmin.num_items_per_page',	50);
+	Config::set('adynadmin.num_items_per_page',	25);
 	Config::set('general.site_title',			'aDynAdmin');
 	Config::set('general.site_tagline',			'A Dynamic MySQL Database Admin.');
 ?>
