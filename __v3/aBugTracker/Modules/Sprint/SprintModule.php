137264f473fe9da1bbb7549c0d9766e14ec6dae4
Andreas Lagerkvist <powerbuoy@gmail.com>
2010-03-25 21:02:40 +0000

    Some Sprint work on BT

--- __v3/aBugTracker/Modules/Sprint/SprintModule.php
+++ __v3/aBugTracker/Modules/Sprint/SprintModule.php
@@ -10,6 +10,16 @@
 
 			self::$tplVars['sprint']['tasks'] = BTTasks::getBySprintsID(self::$tplVars['sprint']['bt_sprints_id']);
 			self::$tplVars['sprint']['days'] = BTSprints::getSprintDays(self::$tplVars['sprint'], self::$tplVars['sprint']['tasks']);
+
+			$numFinishedTasks = 0;
+
+			foreach (self::$tplVars['sprint']['days'] as $day) {
+				if ($day['date'] <= date('Y-m-d') and isset($day['finished_tasks'])) {
+					$numFinishedTasks += count($day['finished_tasks']);
+				}
+			}
+
+			self::$tplVars['sprint']['num_finished_tasks'] = $numFinishedTasks;
 		}
 	}
 ?>
