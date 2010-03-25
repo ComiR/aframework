a82d2bc7ced924e024cbe99ee62032b1a4ddd1db
Andreas Lagerkvist <powerbuoy@gmail.com>
2010-03-25 10:51:36 +0000

    Newly generated lang

--- __v3/aBugTracker/Lang/en.php
+++ __v3/aBugTracker/Lang/en.php
@@ -4,11 +4,26 @@
 'url.add-task' => 'add-task', 
 
 # Task.tpl.php
-'Tasks for PROJECT' => 'Tasks for %0', 
-'Date' => 'Date', 
+'Created by' => 'Created by', 
+'E-mail not revealed' => 'E-mail not revealed', 
+'Fixed by' => 'Fixed by', 
+'Assigned to' => 'Assigned to', 
+'nobody' => 'nobody', 
 'Priority' => 'Priority', 
 'State' => 'State', 
-'Category' => 'Category', 
+'Sprint' => 'Sprint', 
+'Update this Task' => 'Update this Task', 
+
+# TaskModule.php
+'No Sprint' => 'No Sprint', 
+'Idea' => 'Idea', 
+'Must Have' => 'Must Have', 
+'Urgent' => 'Urgent', 
+'New' => 'New', 
+'In Progress' => 'In Progress', 
+'Done' => 'Done', 
+'Add a Comment' => 'Add a Comment', 
+'Your E-mail' => 'Your E-mail', 
 
 # AfterProjectsAdmin.tpl.php
 'Add a Project' => 'Add a Project', 
@@ -21,7 +36,7 @@
 'All Projects' => 'All Projects', 
 
 # Sprint.js
-'Show tasks in this sprint' => 'Show tasks in this sprint', 
+'Show unfinished tasks in this sprint' => 'Show unfinished tasks in this sprint', 
 "This day hasn't happened yet." => "This day hasn't happened yet.", 
 'NUM% on day NUM' => '%0% on day %1', 
 'Day NUM' => 'Day %0', 
@@ -31,29 +46,35 @@
 'Old Sprint:' => 'Old Sprint:', 
 'From START to END' => 'From %0 to %1', 
 'Days in Sprint' => 'Days in Sprint', 
-'NUM% completed on day NUM with NUM fixed tasks (NUM fixed in total now).' => '%0% completed on day %1 with %2 fixed tasks (%3 fixed in total now).', 
+'NUM% completed on day NUM with NUM fixed tasks. NUM/NUM fixed in total now.' => '%0% completed on day %1 with %2 fixed tasks. %3/%4 fixed in total now.', 
 'Tasks to be Completed' => 'Tasks to be Completed', 
 
 # Tasks.tpl.php
-'Add Task' => 'Add Task', 
+'Tasks for PROJECT' => 'Tasks for %0', 
+'Add a Task to PROJECT' => 'Add a Task to %0', 
+'There are no tasks for this project. Click the link above to add one.' => 'There are no tasks for this project. Click the link above to add one.', 
+'Urgent Tasks' => 'Urgent Tasks', 
 'Title' => 'Title', 
 'Content' => 'Content', 
+'Date' => 'Date', 
+"Must Have's" => "Must Have's", 
+'Ideas' => 'Ideas', 
+
+# TasksModule.php
 
 # Tasks.js
+'Show all tasks' => 'Show all tasks', 
 'Hide finished tasks' => 'Hide finished tasks', 
 
 # AddTask.tpl.php
+'Add Task' => 'Add Task', 
+'You are adding a task to PROJECT. Simply fill out the form and your task will be added immediately.' => 'You are adding a task to %0. Simply fill out the form and your task will be added immediately.', 
+'Make sure you use a valid e-mail in case clarifications are needed.' => 'Make sure you use a valid e-mail in case clarifications are needed.', 
 
 # AddTaskModule.php
+'Add Task to PROJECT' => 'Add Task to %0', 
 'Title of Task' => 'Title of Task', 
-'Idea' => 'Idea', 
-'Must Have' => 'Must Have', 
-'Immediately' => 'Immediately', 
-'New' => 'New', 
-'In Progress' => 'In Progress', 
-'Done' => 'Done', 
 'Description' => 'Description', 
-'Your E-mail' => 'Your E-mail', 
 
 # ThankYou.tpl.php
 'Thank You' => 'Thank You', 
