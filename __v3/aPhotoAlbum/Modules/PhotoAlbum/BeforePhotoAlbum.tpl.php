4a9edb98a4a09b0e477f38da09ea63c37a53cfe8
Andreas Lagerkvist <powerbuoy@gmail.com>
2010-03-25 17:07:17 +0000

    breadcrumb-headings in photoalbum

--- __v3/aPhotoAlbum/Modules/PhotoAlbum/BeforePhotoAlbum.tpl.php
+++ __v3/aPhotoAlbum/Modules/PhotoAlbum/BeforePhotoAlbum.tpl.php
@@ -1,1 +1,5 @@
-<h2><?php echo escHTML($album['title']); ?></h2>
+<h2>
+	<a href="<?php echo Router::urlFor('PhotoAlbums'); ?>">
+		<?php echo Lang::get('All Photo Albums'); ?>
+	</a> &rarr; <?php echo escHTML($album['title']); ?>
+</h2>
