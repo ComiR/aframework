96d4e08ba7e886f0c8c743fa69eca7615ac18d72
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2010-03-25 18:44:27 +0000

    Sorting of task tables + moved "Day NUM" beneath sprint table

--- trunk/__v3/aBugTracker/Modules/Tasks/Tasks.tpl.php
+++ trunk/__v3/aBugTracker/Modules/Tasks/Tasks.tpl.php
@@ -17,24 +17,28 @@
 	<h3><?php echo Lang::get('Urgent Tasks'); ?> (<?php echo count($urgent_tasks); ?>)</h3>
 
 	<table class="urgent">
-		<tr>
-			<th><?php echo Lang::get('Title'); ?></th>
-			<th title="Bug ID">#</th>
-			<th><?php echo Lang::get('Content'); ?></th>
-			<th><?php echo Lang::get('Date'); ?></th>
-			<th><?php echo Lang::get('Sprint'); ?></th>
-			<th><?php echo Lang::get('State'); ?></th>
-		</tr>
-		<?php foreach ($urgent_tasks as $task) { ?>
-			<tr<?php if ($task['state'] == 'Done') { ?> class="done"<?php } ?>>
-				<td><a href="<?php echo Router::urlFor('Task', $task); ?>"><?php echo escHTML($task['title']); ?></a></td>
-				<td><?php echo $task['bt_tasks_id']; ?></td>
-				<td><?php echo escHTML(substr($task['content'], 0, 50)); ?>...</td>
-				<td><?php echo date('F jS H:i', strtotime($task['pub_date'])); ?></td>
-				<td><?php echo $task['sprint_id'] ? escHTML($task['sprint_title']) : Lang::get('No Sprint'); ?></td>
-				<td><?php echo Lang::get($task['state']); ?></td>
+		<thead>
+			<tr>
+				<th><?php echo Lang::get('Title'); ?></th>
+				<th title="Bug ID">#</th>
+				<th><?php echo Lang::get('Content'); ?></th>
+				<th><?php echo Lang::get('Date'); ?></th>
+				<th><?php echo Lang::get('Sprint'); ?></th>
+				<th><?php echo Lang::get('State'); ?></th>
 			</tr>
-		<?php } ?>
+		</thead>
+		<tbody>
+			<?php foreach ($urgent_tasks as $task) { ?>
+				<tr<?php if ($task['state'] == 'Done') { ?> class="done"<?php } ?>>
+					<td><a href="<?php echo Router::urlFor('Task', $task); ?>"><?php echo escHTML($task['title']); ?></a></td>
+					<td><?php echo $task['bt_tasks_id']; ?></td>
+					<td><?php echo escHTML(substr($task['content'], 0, 50)); ?>...</td>
+					<td><?php echo date('F jS H:i', strtotime($task['pub_date'])); ?></td>
+					<td><?php echo $task['sprint_id'] ? escHTML($task['sprint_title']) : Lang::get('No Sprint'); ?></td>
+					<td><?php echo Lang::get($task['state']); ?></td>
+				</tr>
+			<?php } ?>
+		</tbody>
 	</table>
 <?php } ?>
 
@@ -42,24 +46,28 @@
 	<h3><?php echo Lang::get("Must Have's"); ?> (<?php echo count($must_have_tasks); ?>)</h3>
 
 	<table class="must-have">
-		<tr>
-			<th><?php echo Lang::get('Title'); ?></th>
-			<th title="Bug ID">#</th>
-			<th><?php echo Lang::get('Content'); ?></th>
-			<th><?php echo Lang::get('Date'); ?></th>
-			<th><?php echo Lang::get('Sprint'); ?></th>
-			<th><?php echo Lang::get('State'); ?></th>
-		</tr>
-		<?php foreach ($must_have_tasks as $task) { ?>
-			<tr<?php if ($task['state'] == 'Done') { ?> class="done"<?php } ?>>
-				<td><a href="<?php echo Router::urlFor('Task', $task); ?>"><?php echo escHTML($task['title']); ?></a></td>
-				<td><?php echo $task['bt_tasks_id']; ?></td>
-				<td><?php echo escHTML(substr($task['content'], 0, 50)); ?>...</td>
-				<td><?php echo date('F jS H:i', strtotime($task['pub_date'])); ?></td>
-				<td><?php echo $task['sprint_id'] ? escHTML($task['sprint_title']) : Lang::get('No Sprint'); ?></td>
-				<td><?php echo Lang::get($task['state']); ?></td>
+		<thead>
+			<tr>
+				<th><?php echo Lang::get('Title'); ?></th>
+				<th title="Bug ID">#</th>
+				<th><?php echo Lang::get('Content'); ?></th>
+				<th><?php echo Lang::get('Date'); ?></th>
+				<th><?php echo Lang::get('Sprint'); ?></th>
+				<th><?php echo Lang::get('State'); ?></th>
 			</tr>
-		<?php } ?>
+		</thead>
+		<tbody>
+			<?php foreach ($must_have_tasks as $task) { ?>
+				<tr<?php if ($task['state'] == 'Done') { ?> class="done"<?php } ?>>
+					<td><a href="<?php echo Router::urlFor('Task', $task); ?>"><?php echo escHTML($task['title']); ?></a></td>
+					<td><?php echo $task['bt_tasks_id']; ?></td>
+					<td><?php echo escHTML(substr($task['content'], 0, 50)); ?>...</td>
+					<td><?php echo date('F jS H:i', strtotime($task['pub_date'])); ?></td>
+					<td><?php echo $task['sprint_id'] ? escHTML($task['sprint_title']) : Lang::get('No Sprint'); ?></td>
+					<td><?php echo Lang::get($task['state']); ?></td>
+				</tr>
+			<?php } ?>
+		</tbody>
 	</table>
 <?php } ?>
 
@@ -67,23 +75,27 @@
 	<h3><?php echo Lang::get('Ideas'); ?> (<?php echo count($idea_tasks); ?>)</h3>
 
 	<table class="ideas">
-		<tr>
-			<th><?php echo Lang::get('Title'); ?></th>
-			<th title="Bug ID">#</th>
-			<th><?php echo Lang::get('Content'); ?></th>
-			<th><?php echo Lang::get('Date'); ?></th>
-			<th><?php echo Lang::get('Sprint'); ?></th>
-			<th><?php echo Lang::get('State'); ?></th>
-		</tr>
-		<?php foreach ($idea_tasks as $task) { ?>
-			<tr<?php if ($task['state'] == 'Done') { ?> class="done"<?php } ?>>
-				<td><a href="<?php echo Router::urlFor('Task', $task); ?>"><?php echo escHTML($task['title']); ?></a></td>
-				<td><?php echo $task['bt_tasks_id']; ?></td>
-				<td><?php echo escHTML(substr($task['content'], 0, 50)); ?>...</td>
-				<td><?php echo date('F jS H:i', strtotime($task['pub_date'])); ?></td>
-				<td><?php echo $task['sprint_id'] ? escHTML($task['sprint_title']) : Lang::get('No Sprint'); ?></td>
-				<td><?php echo Lang::get($task['state']); ?></td>
+		<thead>
+			<tr>
+				<th><?php echo Lang::get('Title'); ?></th>
+				<th title="Bug ID">#</th>
+				<th><?php echo Lang::get('Content'); ?></th>
+				<th><?php echo Lang::get('Date'); ?></th>
+				<th><?php echo Lang::get('Sprint'); ?></th>
+				<th><?php echo Lang::get('State'); ?></th>
 			</tr>
-		<?php } ?>
+		</thead>
+		<tbody>
+			<?php foreach ($idea_tasks as $task) { ?>
+				<tr<?php if ($task['state'] == 'Done') { ?> class="done"<?php } ?>>
+					<td><a href="<?php echo Router::urlFor('Task', $task); ?>"><?php echo escHTML($task['title']); ?></a></td>
+					<td><?php echo $task['bt_tasks_id']; ?></td>
+					<td><?php echo escHTML(substr($task['content'], 0, 50)); ?>...</td>
+					<td><?php echo date('F jS H:i', strtotime($task['pub_date'])); ?></td>
+					<td><?php echo $task['sprint_id'] ? escHTML($task['sprint_title']) : Lang::get('No Sprint'); ?></td>
+					<td><?php echo Lang::get($task['state']); ?></td>
+				</tr>
+			<?php } ?>
+		</tbody>
 	</table>
 <?php } ?>
