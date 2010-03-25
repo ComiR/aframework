c33190d7329dc2ce822cf6305e730ffbe304cc35
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2010-03-25 17:07:17 +0000

    breadcrumb-headings in photoalbum

--- trunk/__v3/aPhotoAlbum/Modules/Photo/BeforePhoto.tpl.php
+++ trunk/__v3/aPhotoAlbum/Modules/Photo/BeforePhoto.tpl.php
@@ -1,4 +1,7 @@
 <h2>
+	<a href="<?php echo Router::urlFor('PhotoAlbums'); ?>">
+		<?php echo Lang::get('All Photo Albums'); ?>
+	</a> &rarr; 
 	<a href="<?php echo Router::urlFor('PhotoAlbum', $photo); ?>">
 		<?php echo escHTML($photo['album_title']); ?>
 	</a> &rarr; <?php echo escHTML($photo['title']); ?>
