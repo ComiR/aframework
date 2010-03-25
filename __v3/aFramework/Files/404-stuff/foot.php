d96bf57a6692bbf4141ce9897b04fa46ca38a20e
Andreas Lagerkvist <powerbuoy@gmail.com>
2010-03-25 10:56:15 +0000

    Debug on 404 page if SU

--- __v3/aFramework/Files/404-stuff/foot.php
+++ __v3/aFramework/Files/404-stuff/foot.php
@@ -1,13 +1,26 @@
-		<form method="get" action="<?php echo Router::urlFor('SearchResults'); ?>">
+			<form method="get" action="<?php echo Router::urlFor('SearchResults'); ?>">
 
-			<p>
-				<label for="q">Try a different search</label> <input type="text" name="q" id="q" /> <input type="submit" value="Find" />
-			</p>
+				<p>
+					<label for="q">Try a different search</label> <input type="text" name="q" id="q" /> <input type="submit" value="Find" />
+				</p>
 
-		</form>
+			</form>
 		
-		<p><small>The request for <?php echo $_SERVER['REQUEST_URI']; ?> returned a 404 header, this could be either from a module forcing a 404-header or simply because the router couldn't match a route.</small></p>
-	
+			<p><small>The request for <?php echo $_SERVER['REQUEST_URI']; ?> returned a 404 header, this could be either from a module forcing a 404-header or simply because the router couldn't match a route.</small></p>
+
+		</div>
+
+		<?php if (SU) { ?>{DEBUG}<?php } ?>
+
+		<script type="text/javascript">
+			aFramework = {modules: []};
+			<?php
+				include DOCROOT . 'aFramework/Modules/Base/1-jquery-1.4.2.js';
+				include DOCROOT . 'aFramework/Modules/Debug/Debug.js';
+			?>
+			aFramework.modules.Debug.run();
+		</script>
+
 	</body>
 
 </html>
