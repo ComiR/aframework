6641acd4cc58a23cd795e5946a5dbbfe767de116
Andreas Lagerkvist <powerbuoy@gmail.com>
2010-04-03 04:44:24 +0000

    future-classes for future articles

--- __v3/aBlog/Modules/RecentArticles/RecentArticles.tpl.php
+++ __v3/aBlog/Modules/RecentArticles/RecentArticles.tpl.php
@@ -1,6 +1,6 @@
 <ol>
 	<?php foreach ($articles as $article) { ?>
-		<li>
+		<li<?php if ($article['future']) { ?> class="future"<?php } ?>>
 			<a href="<?php echo Router::urlFor('Article', $article); ?>">
 				<?php echo escHTML($article['title']); ?>
 			</a>
