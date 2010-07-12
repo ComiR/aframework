11bd56bcb21530c68278f5e0da707da34ef92724
agnesekman <agnesekman@18b212f6-d843-0410-8e89-5b5f2422cd04>
2010-07-12 22:22:38 +0000

    Added routes, controllers and navigation-items for offices, users and objects

--- trunk/__v3/BudhistorikSE/Routes.php
+++ trunk/__v3/BudhistorikSE/Routes.php
@@ -1,3 +1,9 @@
 <?php return array(
-	'/login/'	=> 'UserLogin'
+	'/login/'				=> 'UserLogin', 	# The login-page for users
+	'/kontor/'				=> 'Offices', 		# Every registered office
+	'/kontor/:offices_id/'	=> 'Office', 		# A specific office (with all its users and objects)
+	'/objekt/'				=> 'Objects', 		# Every registered object (except inactive ones)
+	'/objekt/:objects_id/'	=> 'Object', 		# A specific object (with all its bids and its user)
+	'/maklare/'				=> 'Users', 		# Every registered 'user' (real estate agent)
+	'/maklare/:users_id/'	=> 'User'			# A specific user (with its office and objects)
 ); ?>
