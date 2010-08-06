190338d6b37a832a37e1f0977fe3395bda5d612c
agnesekman <agnes.ekman@gmail.com>
2010-08-06 15:22:48 +0000

    Admin Controller Routes

--- __v3/BudhistorikSE/Routes.php
+++ __v3/BudhistorikSE/Routes.php
@@ -1,9 +1,14 @@
 <?php return array(
-	'/login/'						=> 'UserLogin', 	# The login-page for users
 	'/kontor/'						=> 'Offices', 		# Every registered office
 	'/kontor/:offices_id([0-9]+)/'	=> 'Office', 		# A specific office (with all its users and objects)
 	'/objekt/'						=> 'Objects', 		# Every registered object (except inactive ones)
 	'/objekt/:objects_id([0-9]+)/'	=> 'Object', 		# A specific object (with all its bids and its user)
 	'/maklare/'						=> 'Users', 		# Every registered 'user' (real estate agent)
-	'/maklare/:users_id([0-9]+)/'	=> 'User'			# A specific user (with its office and objects)
+	'/maklare/:users_id([0-9]+)/'	=> 'User',			# A specific user (with its office and objects)
+
+	# Admin
+	'/konto/'							=> 'UserLogin', 		# The login-page for users
+	'/konto/objekt/'					=> 'UserObjectsAdmin',	# The 'edit all objects'-page
+	'/konto/objekt/:objects_id([0-9]+)'	=> 'UserObjectAdmin',	# The 'edit one object' page
+	'/konto/objekt/nytt/'				=> 'AddUserObjectAdmin'	# The 'add an object'-page
 ); ?>
