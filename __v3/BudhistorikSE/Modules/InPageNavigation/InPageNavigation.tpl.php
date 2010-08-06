9dc765c87432d842f57989f5a868cd16687c8db5
agnesekman <agnes.ekman@gmail.com>
2010-08-06 12:09:45 +0000

    BHSE

--- __v3/BudhistorikSE/Modules/InPageNavigation/InPageNavigation.tpl.php
+++ __v3/BudhistorikSE/Modules/InPageNavigation/InPageNavigation.tpl.php
@@ -1,5 +1,25 @@
-<ul>
-	<li><a href="#wrapper">Start</a></li>
-	<li><a href="#page">Om oss</a></li>
-	<li><a href="#contact">Kontakt</a></li>
-</ul>
+<?php if (isset(Router::$params['objects_id'])) { ?>
+	<ul>
+		<li><a href="#wrapper">Start</a></li>
+		<li><a href="#object-user">Mäklare</a></li>
+		<li><a href="#object-bids">Budhistorik</a></li>
+	</ul>
+<?php } elseif (isset(Router::$params['offices_id'])) { ?>
+	<ul>
+		<li><a href="#wrapper">Start</a></li>
+		<li><a href="#office-users">Mäklare</a></li>
+		<li><a href="#office-objects">Objekt</a></li>
+	</ul>
+<?php } elseif (isset(Router::$params['users_id'])) { ?>
+	<ul>
+		<li><a href="#wrapper">Start</a></li>
+		<li><a href="#user-office">Kontor</a></li>
+		<li><a href="#user-objects">Objekt</a></li>
+	</ul>
+<?php } else { ?>
+	<ul>
+		<li><a href="#wrapper">Start</a></li>
+		<li><a href="#page">Om oss</a></li>
+		<li><a href="#contact">Kontakt</a></li>
+	</ul>
+<?php } ?>
