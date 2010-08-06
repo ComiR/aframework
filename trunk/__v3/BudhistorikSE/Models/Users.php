fe67a37fbbb000ca4f92186283f1cd76d7ae7c01
agnesekman <agnesekman@18b212f6-d843-0410-8e89-5b5f2422cd04>
2010-08-06 14:24:47 +0000

    Lite trixande

--- trunk/__v3/BudhistorikSE/Models/Users.php
+++ trunk/__v3/BudhistorikSE/Models/Users.php
@@ -12,8 +12,8 @@
 			return self::get('first_name', 'ASC', 0, 1, 'username = "' . escSQL($username) . '" AND password = "' . escSQL($password) . '"');
 		}
 
-		public static function getById($id) {
-			return self::get('first_name', 'ASC', 0, INFINITY, 'users_id = ' . $id);
+		public static function getById ($id) {
+			return self::get('first_name', 'ASC', 0, 1, 'users_id = ' . escSQL($id));
 		}
 
 		public static function get ($sort = 'first_name', $order = 'ASC', $start = 0, $limit = INFINITY, $where = '1 = 1', $select = '1') {
