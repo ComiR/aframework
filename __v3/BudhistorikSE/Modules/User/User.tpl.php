78a63187cce3e82e4cfb74b9fbf917b0fa1c537c
agnesekman <agnes.ekman@gmail.com>
2010-08-06 14:24:47 +0000

    Lite trixande

--- __v3/BudhistorikSE/Modules/User/User.tpl.php
+++ __v3/BudhistorikSE/Modules/User/User.tpl.php
@@ -1,13 +1,5 @@
-<h2>Mäklare</h2>
+<h2>
+	<?php echo escHTML($user['first_name']); ?> <?php echo escHTML($user['last_name']); ?>
+</h2>
 
-<ul>
-	<li>
-		<h3>
-			<a href="<?php echo Router::urlFor('User', $user); ?>">
-				<?php echo escHTML($user['first_name']); ?> <?php echo escHTML($user['last_name']); ?>
-			</a>
-		</h3>
-
-		<?php echo NiceString::makeNice($user['description'], 4, false, 150); ?>
-	</li>
-</ul>
+<?php echo NiceString::makeNice($user['description'], 4, false, 150); ?>
--- __v3/BudhistorikSE/Routes.php
+++ __v3/BudhistorikSE/Routes.php
@@ -1,9 +1,9 @@
 <?php return array(
-	'/login/'				=> 'UserLogin', 	# The login-page for users
-	'/kontor/'				=> 'Offices', 		# Every registered office
-	'/kontor/:offices_id/'	=> 'Office', 		# A specific office (with all its users and objects)
-	'/objekt/'				=> 'Objects', 		# Every registered object (except inactive ones)
-	'/objekt/:objects_id/'	=> 'Object', 		# A specific object (with all its bids and its user)
-	'/maklare/'				=> 'Users', 		# Every registered 'user' (real estate agent)
-	'/maklare/:users_id/'	=> 'User'			# A specific user (with its office and objects)
+	'/login/'						=> 'UserLogin', 	# The login-page for users
+	'/kontor/'						=> 'Offices', 		# Every registered office
+	'/kontor/:offices_id([0-9]+)/'	=> 'Office', 		# A specific office (with all its users and objects)
+	'/objekt/'						=> 'Objects', 		# Every registered object (except inactive ones)
+	'/objekt/:objects_id([0-9]+)/'	=> 'Object', 		# A specific object (with all its bids and its user)
+	'/maklare/'						=> 'Users', 		# Every registered 'user' (real estate agent)
+	'/maklare/:users_id([0-9]+)/'	=> 'User'			# A specific user (with its office and objects)
 ); ?>
