f86973d50019b2162533cfacf882b1f4eaa6f3af
Andreas Lagerkvist <powerbuoy@gmail.com>
2010-09-29 20:19:24 +0000

    BHSE Config

--- __v3/BudhistorikSE/Config.php
+++ __v3/BudhistorikSE/Config.php
@@ -1,31 +1,21 @@
 <?php
 	Config::set('db.user',						'root');
-	Config::set('db.pass',						'root');
+	Config::set('db.pass',						'');
 	Config::set('db.host',						'localhost');
-	Config::set('db.name',						'budhistorik');
+	Config::set('db.name',						'budhistorik_se');
 
-	Config::set('lang.allowed_langs',			'sv');
-	Config::set('lang.default_lang',			'sv');
-
-	Config::set('general.default_style',		'budhistorik');
-
-	Config::set('general.site_author',			'Svensk Budcertifiering AB');
-	Config::set('general.site_title',			'Budhistorik.se');
-	Config::set('general.site_tagline',			'Här blir det någon form av tagline');
-
-	Config::set('general.contact_email',		'info@budhistorik.se');
-	Config::set('general.date_format',			'l jS \of F Y');
-	Config::set('general.ga_id',				false);
-
-	Config::set('ie_support.script_support',	false);
-	Config::set('ie_support.style_support',		true);
+	Config::set('general.default_style',				'budhistorik');
+	Config::set('general.site_author',				'Erik Byström');
+	Config::set('general.site_title',				'Budhistorik.se');
+	Config::set('general.site_tagline',				'En tagline');
+	Config::set('general.contact_email',				'info@budhistorik.se');
+	Config::set('general.date_format',				'l jS \of F Y');
+	Config::set('general.ga_id',					false);
 
 	Config::set('admin.user',					'admin');
 	Config::set('admin.pass',					'1234');
 	Config::set('su.user',						'su');
 	Config::set('su.pass',						'4321');
-
-	Config::set('navigation.home',				true);
+	
 	Config::set('navigation.styles',			false);
-
 ?>
