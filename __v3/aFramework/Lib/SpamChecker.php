c7fd81523757982a68cbec76af1143aa35537288
Andreas Lagerkvist <powerbuoy@gmail.com>
2011-01-04 11:35:13 +0000

    SpamChecker fixes + updated super simple tabs

--- __v3/aFramework/Lib/SpamChecker.php
+++ __v3/aFramework/Lib/SpamChecker.php
@@ -35,7 +35,6 @@
 				"Good crew it's cool :)", 
 				'This site is crazy :)', 
 				'Jonny was here', 
-				"Good crew it's cool :)", 
 				'very best job', 
 				'real beauty page', 
 				'Thanks funny site', 
@@ -46,51 +45,43 @@
 				'Best Site Good Work', 
 				'good material thanks', 
 				'very best job', 
-				"Good crew it's cool :)", 
-				'This site is crazy :)', 
 				'Very interesting tale', 
 				'Excellent work, Nice Design', 
 				'Best Site good looking', 
 				'Thanks funny site', 
 				"It's funny goodluck", 
-				"Good crew it's cool :)", 
 				"It's serious", 
 				'Very Good Site', 
 				'Excellent work, Nice Design', 
 				'Cool site goodluck :)', 
-				'Best Site good looking', 
-				'Best Site Good Work', 
-				'Very interesting tale', 
 				"It's serious", 
-				'magic story very thanks', 
-				'Best Site good looking', 
+				'magic story very thanks',
 				"I'm happy very good site", 
-				'Best Site good looking', 
 				'Very funny pictures', 
 				"It's funny goodluck", 
 				'Cool site goodluck :)', 
-				'Cool site goodluck :)', 
 				'Excellent work, Nice Design', 
 				'perfect design thanks', 
-				'Best Site Good Work', 
 				'this is be cool 8)', 
-				'Wonderfull great site', 
-				"Good crew it's cool :)", 
+				'Wonderfull great site',  
 				'Hello good day', 
-				'this is be cool 8)', 
 				'Cool site goodluck :)', 
-				"I'm happy very good site", 
 				'this post is fantastic', 
 				'Very interesting tale', 
 				'Gloomy tales', 
 				'Best Site Good Work', 
 				'Thanks funny site', 
-				'perfect design thanks', 
-				'this post is fantastic'
+				'perfect design thanks'
 			);
 
 			if (in_array($fields['content'], $alwaysSpam)) {
 				$score -= 10;
+			}
+
+			#####
+			# Since I use markdown regular links are very likely spam
+			if (preg_match('/<a href=".*?">.*?<\/a>/', $fields['content'])) {
+				$score -= 5;
 			}
 
 			#####
