3f2bf8e610f5aef55129c4d13a55f4ac2c286a06
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2011-01-04 11:35:13 +0000

    SpamChecker fixes + updated super simple tabs

--- trunk/__v3/aFramework/Modules/Base/jquery.superSimpleTabs.js
+++ trunk/__v3/aFramework/Modules/Base/jquery.superSimpleTabs.js
@@ -3,7 +3,7 @@
 Super Simple Tabs
 
 @version:
-1.1
+1.3
 
 @author:
 Andreas Lagerkvist
@@ -24,7 +24,7 @@ http://creativecommons.org/licenses/by/3.0/
 jquery
 
 @does:
-This is an extremely basic tabs-plugin which allows you to create tabbed content from the ever-so-common list of in-page-links. The plug-in takes one argument; 'selected', which allows you to set which tab should be selected by default. It defaults to the first tab in the list.
+This is an extremely basic tabs-plugin which allows you to create tabbed content from the ever-so-common list of in-page-links. You can set the selected tab, the show and hide animation as well as the duration through the config argument.
 
 @howto:
 jQuery('ul.tabs').superSimpleTabs(); would make every ul with the class 'tabs' hide show the content its links are pointing to.
@@ -48,19 +48,24 @@ jQuery('ul.tabs').superSimpleTabs(); would make every ul with the class 'tabs' h
 @exampleJS:
 jQuery('#jquery-super-simple-tabs-example ul').superSimpleTabs();
 ***/
-jQuery.fn.superSimpleTabs = function (selected) {
-	var sel = selected || 1;
+jQuery.fn.superSimpleTabs = function (conf) {
+	var config = jQuery.extend({
+		selected: 1, 
+		show: 'show', 
+		hide: 'hide', 
+		duration: 0
+	}, conf);
 
 	return this.each(function () {
 		var ul	= jQuery(this);
-		var ipl	= 'a[href^=#]';
+		var ipl	= 'a[href^="#"]';
 
 		// Go through all the in-page links in the ul
 		// and hide all but the selected's contents
 		ul.find(ipl).each(function (i) {
 			var link = jQuery(this);
 
-			if ((i + 1) === sel) {
+			if ((i + 1) === config.selected) {
 				link.addClass('selected');
 			}
 			else {
@@ -88,10 +93,15 @@ jQuery.fn.superSimpleTabs = function (selected) {
 				var selected = ul.find('a.selected');
 
 				if (selected.length) {
-					jQuery(selected.removeClass('selected').attr('href')).hide();
-				}
+					jQuery(selected.removeClass('selected').attr('href'))[config.hide](config.duration, function () {
+						jQuery(link.attr('href'))[config.show](config.duration);
+					});
 
-				jQuery(link.addClass('selected').attr('href')).show();
+					link.addClass('selected');
+				}
+				else {
+					jQuery(link.addClass('selected').attr('href'))[config.show](config.duration);
+				}
 
 				return false;
 			}
