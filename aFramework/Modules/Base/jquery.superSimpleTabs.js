e8edee7f8ba3cc89413028a2974b88a37160fa0a
Andreas Lagerkvist <powerbuoy@gmail.com>
2012-04-02 06:30:32 +0000

    Improvements to superSimpleTabs

--- aFramework/Modules/Base/jquery.superSimpleTabs.js
+++ aFramework/Modules/Base/jquery.superSimpleTabs.js
@@ -3,13 +3,14 @@
 Super Simple Tabs
 
 @version:
-1.3
+1.4
 
 @author:
 Andreas Lagerkvist
 
 @date:
 2009-09-17
+2012-02-29
 
 @url:
 http://andreaslagerkvist.com/jquery/super-simple-tabs/
@@ -50,10 +51,11 @@ jQuery('#jquery-super-simple-tabs-example ul').superSimpleTabs();
 ***/
 jQuery.fn.superSimpleTabs = function (conf) {
 	var config = jQuery.extend({
-		selected: 1, 
-		show: 'show', 
-		hide: 'hide', 
-		duration: 0
+		selected:	1, 				// Which tab is initially selected (hash overrides this)
+		show:		'show', 		// Show animation
+		hide:		'hide', 		// Hide animation
+		duration:	0,				// Animation duration
+		callback:	function () {}	// Callback after tab has been clicked
 	}, conf);
 
 	return this.each(function () {
@@ -89,22 +91,65 @@ jQuery.fn.superSimpleTabs = function (conf) {
 				}
 			}
 
+			// Only continue if the clicked element was an in page link
 			if (link) {
 				var selected = ul.find('a.selected');
 
 				if (selected.length) {
+					// Remove currently .selected, hide the element it was pointing to
 					jQuery(selected.removeClass('selected').attr('href'))[config.hide](config.duration, function () {
-						jQuery(link.attr('href'))[config.show](config.duration);
+						// Then show the element the clicked link was pointing to
+						jQuery(link.attr('href'))[config.show](config.duration, function () {
+							config.callback(link.attr('href'));
+						});
 					});
 
 					link.addClass('selected');
 				}
 				else {
-					jQuery(link.addClass('selected').attr('href'))[config.show](config.duration);
+					jQuery(link.addClass('selected').attr('href'))[config.show](config.duration, function () {
+						config.callback(link.attr('href'));
+					});
 				}
+
+				// Update the hash
+				superSimpleTabsUpdateHash(link.attr('href'));
 
 				return false;
 			}
 		});
+
+		// If a hash is set, click that tab
+		var hash = window.location.hash;
+
+		if (hash) {
+			// We can't simply .click() the link since that will run the show/hide animation
+			jQuery(ul.find('a').removeClass('selected').attr('href')).hide();
+			jQuery(ul.find('a[href="' + hash + '"]').addClass('selected').attr('href')).show();
+			config.callback(hash);
+		}
 	});
 };
+
+// http://stackoverflow.com/questions/1489624/modifying-document-location-hash-without-page-scrolling#answer-1489802
+function superSimpleTabsUpdateHash (hash) {
+	hash = hash.replace( /^#/, '' );
+	var fx, node = $( '#' + hash );
+	if ( node.length ) {
+	  fx = $( '<div></div>' )
+		      .css({
+		          position:'fixed',
+		          left:0,
+		          top:0,
+		          visibility:'hidden'
+		      })
+		      .attr( 'id', hash )
+		      .appendTo( document.body );
+	  node.attr( 'id', '' );
+	}
+	document.location.hash = hash;
+	if ( node.length ) {
+	  fx.remove();
+	  node.attr( 'id', hash );
+	}
+}
