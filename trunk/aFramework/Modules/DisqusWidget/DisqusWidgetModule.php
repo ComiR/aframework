c5122945fe11c9d172d9dc22983a5d165408ac3d
powerbuoy <powerbuoy@18b212f6-d843-0410-8e89-5b5f2422cd04>
2012-04-02 07:00:42 +0000

    DisqusWidget

---
+++ trunk/aFramework/Modules/DisqusWidget/DisqusWidgetModule.php
@@ -0,0 +1,17 @@
+<?php
+	class aFramework_DisqusWidgetModule {
+		public static $tplVars = array();
+		public static $tplFile = true;
+
+		public static function run () {
+			$shortname = Config::get('disqus.shortname');
+
+			if ($shortname) {
+				self::$tplVars['shortname'] = $shortname;
+			}
+			else {
+				self::$tplFile = false;
+			}
+		}
+	}
+?>
