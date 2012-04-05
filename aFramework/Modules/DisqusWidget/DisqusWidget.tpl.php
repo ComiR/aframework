91ebc855d5360f82a6bd7014479fb0152a884fc2
Andreas Lagerkvist <powerbuoy@gmail.com>
2012-04-05 02:52:39 +0000

    Use config for num recent Disqus comments

--- aFramework/Modules/DisqusWidget/DisqusWidget.tpl.php
+++ aFramework/Modules/DisqusWidget/DisqusWidget.tpl.php
@@ -1,5 +1,5 @@
 <div id="recentcomments" class="dsq-widget">
 	<h2 class="dsq-widget-title">Recent Comments</h2>
-	<script type="text/javascript" src="http://<?php echo $shortname ?>.disqus.com/recent_comments_widget.js?num_items=5&hide_avatars=0&avatar_size=32&excerpt_length=200"></script>
+	<script type="text/javascript" src="http://<?php echo $shortname ?>.disqus.com/recent_comments_widget.js?num_items=<?php echo $num_items ?>&hide_avatars=0&avatar_size=32&excerpt_length=200"></script>
 </div>
 <a href="http://disqus.com/">Powered by Disqus</a>
